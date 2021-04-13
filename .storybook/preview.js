c3a2dab61e70d41cb2b36c2b5b0f58ba7b439a02
Filip Tammergård <filip@tammergard.se>
2021-04-13 14:51:38 +0200

    chore(deps): bump storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,53 @@
 import "../src/styles/variables.css";
 import "../src/styles/main.css";
 
+const customViewports = {
+  mobile: {
+    name: "Mobile",
+    styles: {
+      width: "360px",
+      height: "812px",
+    },
+  },
+  tabletPortrait: {
+    name: "Tablet portrait",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  tabletLandscape: {
+    name: "Tablet landscape",
+    styles: {
+      width: "1024px",
+      height: "768px",
+    },
+  },
+  desktop: {
+    name: "Desktop",
+    styles: {
+      width: "1440px",
+      height: "900px",
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: { viewports: customViewports },
+  backgrounds: {
+    default: "light",
+    values: [
+      { name: "light", value: "#ffffff" },
+      { name: "dark", value: "#121212" },
+    ],
+  },
+  grid: {
+    cellSize: 10,
+    gridOn: false,
+    columns: 8,
+    gap: "10px",
+    gutter: "15px",
+    maxWidth: "1440px",
+  },
 };
