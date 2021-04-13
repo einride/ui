ac36834b10416cdedff67510299cac618e7eaed5
Filip Tammergård <filip@tammergard.se>
2021-04-13 11:43:09 +0200

    feat: remove lib from gitignore

---
+++ lib/main.d.ts
@@ -0,0 +1,9 @@
+import "./assets/fonts/fonts.css";
+import "./styles/variables.css";
+import "./styles/main.css";
+export { BrandpadButton } from "components/BrandpadButton";
+export type { BrandpadButtonProps } from "components/BrandpadButton";
+export { Button } from "components/Button";
+export type { ButtonProps } from "components/Button";
+export { Input } from "components/Input";
+export type { InputProps } from "components/Input";
