d89b1a021344e9964febed6d1299c9a92ab26c65
Filip Tammergård <filip@tammergard.se>
2021-04-13 11:10:38 +0200

    chore: remove cra stuff

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./assets/fonts/fonts.css";
-import "./styles/variables.css";
-import "./styles/main.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
