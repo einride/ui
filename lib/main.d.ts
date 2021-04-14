5f442efd9731a3212c58e398be4beefb88d53688
Filip Tammergård <filip@tammergard.se>
2021-04-14 11:34:53 +0200

    feat: export button outline

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -3,3 +3,5 @@ import "./styles/variables.css";
 import "./styles/main.css";
 export { Button } from "./components/Button";
 export type { ButtonProps } from "./components/Button";
+export { ButtonOutline } from "./components/ButtonOutline";
+export type { ButtonOutlineProps } from "./components/ButtonOutline";
