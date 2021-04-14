27e2537809994e0a8b31d39cf5a9323434729dc7
Filip Tammergård <filip@tammergard.se>
2021-04-14 11:34:53 +0200

    feat: export button outline

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import "./styles/main.css";
 
 export { Button } from "./components/Button";
 export type { ButtonProps } from "./components/Button";
+
+export { ButtonOutline } from "./components/ButtonOutline";
+export type { ButtonOutlineProps } from "./components/ButtonOutline";
