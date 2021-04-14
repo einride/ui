66cd576523504ed5e0941812b7f231c4cdd6fa27
Filip Tammergård <filip@tammergard.se>
2021-04-14 11:05:35 +0200

    feat: replace placeholder buttons with brandpad ones

--- lib/components/BrandpadButton/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { BrandpadButton } from "./BrandpadButton";
-export type { BrandpadButtonProps } from "./BrandpadButton";
--- lib/components/ButtonOutline/ButtonOutline.d.ts
+++ lib/components/ButtonOutline/ButtonOutline.d.ts
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
-export interface BrandpadButtonProps {
+export interface ButtonOutlineProps {
     children: ReactNode;
     disabled?: boolean;
     onClick: () => void;
 }
-export declare const BrandpadButton: {
-    ({ children, ...props }: BrandpadButtonProps): JSX.Element;
+export declare const ButtonOutline: {
+    ({ children, ...props }: ButtonOutlineProps): JSX.Element;
     defaultProps: {
         disabled: boolean;
     };
