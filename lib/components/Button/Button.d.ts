cc3c7a892e7ccf7ca2e86ff4715230d1a2884082
Filip Tammergård <filip@tammergard.se>
2021-04-14 11:05:35 +0200

    feat: replace placeholder buttons with brandpad ones

--- lib/components/Button/Button.d.ts
+++ lib/components/Button/Button.d.ts
@@ -2,25 +2,11 @@ import { ReactNode } from "react";
 export interface ButtonProps {
     children: ReactNode;
     disabled?: boolean;
-    formId?: string;
-    hasArrow?: boolean;
-    isFullWidth?: boolean;
-    onClick?: () => void;
-    type?: "button" | "submit" | "reset" | undefined;
-}
-export declare const Button: {
-    ({ children, hasArrow, ...props }: ButtonProps): JSX.Element;
-    defaultProps: {
-        disabled: boolean;
-        formId: undefined;
-        hasArrow: boolean;
-        isFullWidth: boolean;
-        onClick: undefined;
-        type: string;
-    };
-};
-export interface BrandpadButtonProps {
-    children: ReactNode;
     onClick: () => void;
 }
-export declare const BrandpadButton: ({ children, ...props }: BrandpadButtonProps) => JSX.Element;
+export declare const Button: {
+    ({ children, ...props }: ButtonProps): JSX.Element;
+    defaultProps: {
+        disabled: boolean;
+    };
+};
