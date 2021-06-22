ce5d8dfaf7f157a6b8ba9261dfeb026368af6a07
Björn Lexell <bjorn.lexell@einride.tech>
2021-06-22 15:37:34 +0200

    feat: add icon button

--- src/components/buttons/BaseButton.tsx
+++ src/components/buttons/BaseButton.tsx
@@ -1,24 +1,32 @@
 /** @jsxImportSource theme-ui */
 import { ButtonHTMLAttributes } from "react";
 
-export interface BaseButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement> {}
+const HEIGHT_PIXELS = 56;
 
-export const BaseButton = (props: BaseButtonProps) => {
+export interface BaseButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Defaults to large */
+  size: "small" | "large";
+}
+
+export const BaseButton = ({ size, ...props }: BaseButtonProps) => {
   return (
     <button
       sx={{
-        borderRadius: "48px",
+        borderRadius: `${HEIGHT_PIXELS}px`,
         backgroundColor: "unset",
         border: "none",
         width: "240px",
-        height: "48px",
+        height: size === "small" ? "48px" : `${HEIGHT_PIXELS}px`,
         padding: "0px 16px",
         cursor: "pointer",
         ":disabled": {
           backgroundColor: "buttons.background.disabled",
           color: "buttons.text.disabled",
           cursor: "not-allowed",
+        },
+        ":disabled svg": {
+          fill: "buttons.text.disabled",
         },
         ":focus": {
           textDecoration: "underline",
