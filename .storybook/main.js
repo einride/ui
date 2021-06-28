1a71fb3a1a7121bbf78348b4f45075f5c9f57237
Filip Tammergård <filip@tammergard.se>
2021-06-28 15:48:06 +0200

    chore: add temporal webpack config to support emotion 11

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: [
     "../src/stories/**/*.stories.mdx",
@@ -10,5 +13,18 @@ module.exports = {
   ],
   typescript: {
     reactDocgen: "none"
-  }
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    }
+  },
 };
