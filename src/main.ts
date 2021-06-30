6c68baa93b5d19b8bf4e6910f6d69b488abe7c50
Filip Tammergård <filip@tammergard.se>
2021-06-30 15:37:31 +0200

    chore: clean main templorarily

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,1 @@
 import "./styles/main.css";
-import "./styles/variables.css";
-
-export { Body } from "./components/typography/Body/Body";
-export type { BodyProps } from "./components/typography/Body/Body";
