b76a576973419ee56ba9f64b5a6ae9fbea81e094
Filip Tammergård <filip@tammergard.se>
2021-06-30 15:39:37 +0200

    fix: fix buttons

---
+++ src/components/buttons/WithIconButton/WithIconButton.stories.tsx
@@ -0,0 +1,37 @@
+import { Story } from "@storybook/react/types-6-0";
+import { WithIconButton, WithIconButtonProps } from "./WithIconButton";
+
+export default {
+  title: "Buttons/WithIconButton",
+  component: WithIconButton,
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+    size: {
+      options: ["small", "large"],
+      control: { type: "radio" },
+    },
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/YYzIjuZlggbzY16y5gGyWB/Components-v1.0?node-id=7%3A74",
+    },
+    docs: {
+      description: {
+        component: "Some component _markdown_",
+      },
+    },
+  },
+};
+
+const Template: Story<WithIconButtonProps> = (args) => (
+  <WithIconButton {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Button",
+};
