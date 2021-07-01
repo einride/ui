01e6a5f078ca974a5978e25dba6612475fb20e01
Filip Tammergård <filip@tammergard.se>
2021-07-01 12:35:00 +0200

    chore: make @emotion/react and @emotion/styled dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,5 @@ module.exports = {
   extends: ["plugin:@einride/default"],
   rules: {
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
-    "import/no-extraneous-dependencies": "off",
   },
 };
