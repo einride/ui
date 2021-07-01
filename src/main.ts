d61ce93f97b0003b28110f4e6f377dfa5417074b
Filip Tammergård <filip@tammergard.se>
2021-07-01 12:28:46 +0200

    chore: export all components in main to expose them in built lib

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import "./styles/main.css";
+
+export { IconButton } from "./components/buttons/IconButton";
+export { PrimaryButton } from "./components/buttons/PrimaryButton";
+export { SecondaryButton } from "./components/buttons/SecondaryButton";
+export { TertiaryButton } from "./components/buttons/TertiaryButton";
+export { WithIconButton } from "./components/buttons/WithIconButton";
+export { Heading } from "./components/typography/Heading";
+export { MegaTitle } from "./components/typography/MegaTitle";
+export { SmallText } from "./components/typography/SmallText";
+export { SubHeading } from "./components/typography/SubHeading";
+export { Text } from "./components/typography/Text";
+export { Title } from "./components/typography/Title";
