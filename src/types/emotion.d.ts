0faec2e8a590b3c095a59590dc097801ff1d79c0
Filip Tammergård <filip@tammergard.se>
2021-07-01 12:26:57 +0200

    chore: add eslint plugin and fix linting

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -2,5 +2,6 @@ import "@emotion/react";
 import type { Theme as ThemeType } from "../theme";
 
 declare module "@emotion/react" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends ThemeType {}
 }
