d25e8ba1360829cab1dd6d34bc1cd9d9232251a1
Filip Tammergård <filip@tammergard.se>
2021-07-05 14:22:42 +0200

    chore: update typography structure

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@emotion/react"
-import "../src/styles/main.css";
+import "../src/assets/fonts/fonts.css"
 import "../src/styles/typography.css";
+import "../src/styles/main.css";
 import { light } from "../src/theme";
 
 const customViewports = {
