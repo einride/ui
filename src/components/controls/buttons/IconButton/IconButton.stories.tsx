797595f23b16356c0f26f4464405c0a4f930f282
Filip Tammergård <filip@tammergard.se>
2021-07-06 10:10:10 +0200

    chore: remove unnessecary argtypes

--- src/components/controls/buttons/IconButton/IconButton.stories.tsx
+++ src/components/controls/buttons/IconButton/IconButton.stories.tsx
@@ -9,13 +9,6 @@ export default {
     disabled: {
       control: "boolean",
     },
-    size: {
-      options: ["small", "large"],
-      control: { type: "radio" },
-    },
-    icon: {
-      control: false,
-    },
   },
   parameters: {
     design: {
