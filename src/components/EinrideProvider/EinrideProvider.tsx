b1b4c7c86716babe97b6bb29f2f6770b2ec98d16
Filip Tammergård <filip@tammergard.se>
2021-07-06 13:44:52 +0200

    fix: make colorMode and customTheme optional

--- src/components/EinrideProvider/EinrideProvider.tsx
+++ src/components/EinrideProvider/EinrideProvider.tsx
@@ -4,20 +4,20 @@ import { themes } from "../../theme";
 
 interface EinrideProviderProps {
   children: ReactNode;
-  colorMode: "light" | "dark";
+  colorMode?: "light" | "dark";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  customTheme: any;
+  customTheme?: any;
 }
 
 export const EinrideProvider = ({
   children,
   colorMode = "light",
-  customTheme,
+  customTheme = {},
 }: EinrideProviderProps) => {
   const defaultTheme = themes[colorMode];
   const theme = {
     ...defaultTheme,
-    custom: customTheme?.[colorMode],
+    custom: customTheme[colorMode],
   };
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
