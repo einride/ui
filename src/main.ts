f54af9510e5e769898afa538ca956fd3275cba9a
Filip Tammergård <filip@tammergard.se>
2021-07-06 10:43:47 +0200

    fix: add einride provider that registers theme

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ export { PrimaryButton } from "./components/controls/buttons/PrimaryButton";
 export { SecondaryButton } from "./components/controls/buttons/SecondaryButton";
 export { TertiaryButton } from "./components/controls/buttons/TertiaryButton";
 export { WithIconButton } from "./components/controls/buttons/WithIconButton";
+export { EinrideProvider } from "./components/EinrideProvider";
 export { VerticalSpacing } from "./components/layout/VerticalSpacing";
 export { Caption } from "./components/typography/Caption";
 export { MegaTitle } from "./components/typography/MegaTitle";
