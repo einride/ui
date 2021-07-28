40121dc768bd1ff278f3be1382e8a4c8f1d219ea
EllenWiderstrand <ellen.widerstrand@einride.tech>
2021-07-28 14:53:49 +0200

    feat: add textarea component

---
+++ src/components/controls/textareas/Textarea/Textarea.tsx
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+import { ChangeEvent } from "react";
+
+const StyledTextarea = styled.textarea`
+  display: block;
+  min-width: 240px;
+  resize: none;
+  background-color: ${({ theme }) => theme.colors.background.secondary};
+  color: ${({ theme }) => theme.colors.content.primary};
+  padding: 12px 16px;
+  border: unset;
+  border-radius: 2px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.content.secondary};
+  }
+
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) => theme.colors.background.tertiary};
+  }
+
+  &:disabled::placeholder {
+    color: ${({ theme }) => theme.colors.content.disabled};
+  }
+
+  &:focus {
+    box-shadow: 0px 0px 0px 1px ${({ theme }) => theme.colors.border.selected}
+      inset;
+    outline: none;
+  }
+`;
+
+const TextAreaWithMargin = styled(StyledTextarea)`
+  margin-top: 3px;
+`;
+
+const StyledLabel = styled.label`
+  color: ${({ theme }) => theme.colors.content.secondary};
+  margin-top: 5px;
+`;
+
+export interface TextareaProps {
+  label?: string;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const Textarea = ({ label, ...props }: TextareaProps) => {
+  if (label) {
+    return (
+      <StyledLabel>
+        {label}
+        <TextAreaWithMargin {...props} />
+      </StyledLabel>
+    );
+  }
+
+  return <StyledTextarea {...props} />;
+};
