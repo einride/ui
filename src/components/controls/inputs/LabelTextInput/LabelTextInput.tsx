2276e8530c117e6df07b39b55e02b5a3dc0ddb09
EllenWiderstrand <ellen.widerstrand@einride.tech>
2021-07-28 09:56:57 +0200

    feat: add new component that uses TextInput but also has a label

---
+++ src/components/controls/inputs/LabelTextInput/LabelTextInput.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import { ChangeEvent, InputHTMLAttributes, ReactNode } from "react";
+import { Theme } from "../../../../theme";
+import { TextInput } from "../TextInput";
+
+const getColor = (theme: Theme, status?: Status) => {
+  switch (status) {
+    case "success":
+      return theme.colors.content.positive;
+    case "fail":
+      return theme.colors.content.negative;
+    default:
+      return theme.colors.content.secondary;
+  }
+};
+
+const StyledLabel = styled.label<{ status?: Status }>`
+  margin: 5px 0 3px;
+  color: ${({ theme, status }) => getColor(theme, status)};
+
+  &:focus-within {
+    color: ${({ theme }) => theme.colors.content.primary};
+  }
+`;
+
+type Status = "success" | "fail";
+
+export interface LabelTextInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  label: ReactNode;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  required?: boolean;
+  status?: Status;
+  statusMessage?: ReactNode;
+  value: string;
+}
+
+export const LabelTextInput = ({
+  label,
+  required,
+  status,
+  ...props
+}: LabelTextInputProps) => {
+  return (
+    <StyledLabel status={status}>
+      {label} {required && " (required)"}
+      <TextInput status={status} {...props} />
+    </StyledLabel>
+  );
+};
---
+++ src/components/controls/inputs/LabelTextInput/index.ts
@@ -0,0 +1,1 @@
+export { LabelTextInput } from "./LabelTextInput";
