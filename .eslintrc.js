386d0d675eabdef1915a920760f5c199ea9e29bd
Filip Tammergård <filip@tammergard.se>
2021-09-03 11:32:17 +0200

    chore: add eslint rule for having react in jsx scope
    To support React 16.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,13 @@ module.exports = {
   rules: {
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
   },
+  overrides: [
+    {
+      files: ["src/*.tsx"],
+      excludedFiles: "*.stories.tsx",
+      rules: {
+        "react/react-in-jsx-scope": "error",
+      },
+    },
+  ],
 };
