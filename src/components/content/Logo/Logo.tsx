0043de0fae6f330c6fc630f8974aca7b9745b274
Markus Pettersson <pmarkus@users.noreply.github.com>
2021-09-03 15:20:06 +0200

    feat: enable animation on logo

--- src/components/content/Logo/Logo.tsx
+++ src/components/content/Logo/Logo.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { forwardRef } from "react";
 import logoDefaultLarge from "../../../assets/logo/logo-default-large.svg";
 import logoDefaultSmall from "../../../assets/logo/logo-default-small.svg";
 import logoInverseLarge from "../../../assets/logo/logo-inverse-large.svg";
@@ -10,24 +11,25 @@ export interface LogoProps {
   variant?: "default" | "inverse";
 }
 
-export const Logo = ({ size = "small", variant }: LogoProps) => {
-  const colorMode = useColorMode();
+export const Logo = forwardRef<HTMLImageElement, LogoProps>(
+  ({ size = "small", variant }, ref) => {
+    const colorMode = useColorMode();
+    const logoDefault = () => {
+      if (size === "small")
+        return <img src={logoDefaultSmall} alt="Einride Logo" ref={ref} />;
+      return <img src={logoDefaultLarge} alt="Einride Logo" ref={ref} />;
+    };
 
-  const logoDefault = () => {
-    if (size === "small")
-      return <img src={logoDefaultSmall} alt="Einride Logo" />;
-    return <img src={logoDefaultLarge} alt="Einride Logo" />;
-  };
+    const logoInverse = () => {
+      if (size === "small")
+        return <img src={logoInverseSmall} alt="Einride Logo" ref={ref} />;
+      return <img src={logoInverseLarge} alt="Einride Logo" ref={ref} />;
+    };
 
-  const logoInverse = () => {
-    if (size === "small")
-      return <img src={logoInverseSmall} alt="Einride Logo" />;
-    return <img src={logoInverseLarge} alt="Einride Logo" />;
-  };
+    if (variant === "inverse" || (!variant && colorMode === "dark")) {
+      return logoInverse();
+    }
 
-  if (variant === "inverse" || (!variant && colorMode === "dark")) {
-    return logoInverse();
+    return logoDefault();
   }
-
-  return logoDefault();
-};
+);
