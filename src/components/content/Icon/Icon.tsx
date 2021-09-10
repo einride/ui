264be944d776c1756e0609d1aedeb8227d8d995f
Filip Tammergård <filip@tammergard.se>
2021-09-10 17:05:35 +0200

    feat: add icon helper function

---
+++ src/components/content/Icon/Icon.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+
+export interface IconProps {
+  name:
+    | "checkmark"
+    | "warning"
+    | "chevronDown"
+    | "chevronUp"
+    | "chevronRight"
+    | "chevronLeft"
+    | "xMark"
+    | "arrowUp"
+    | "arrowDown"
+    | "arrowRightwards"
+    | "arrowLeftwards"
+    | "bolt"
+}
+
+export const Icon = ({ name }: IconProps) => {
+  switch (name) {
+    case "checkmark":
+      return <>✓</>
+    case "warning":
+      return <>⚠︎</>
+    case "chevronDown":
+      return <>⌄</>
+    case "chevronUp":
+      return <>⌃</>
+    case "chevronRight":
+      return <>˃</>
+    case "chevronLeft":
+      return <>˂</>
+    case "xMark":
+      return <>❌</>
+    case "arrowUp":
+      return <>→</>
+    case "arrowDown":
+      return <>↓</>
+    case "arrowRightwards":
+      return <>→</>
+    case "arrowLeftwards":
+      return <>←</>
+    case "bolt":
+      return <>☈</>
+    default:
+      return null
+  }
+}
