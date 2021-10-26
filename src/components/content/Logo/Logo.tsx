c0b336f5b456f3fe1e0f74953b8b52d4088f4473
Filip Tammergård <filip@tammergard.se>
2021-10-26 10:51:16 +0200

    fix: extend logo props

--- src/components/content/Logo/Logo.tsx
+++ src/components/content/Logo/Logo.tsx
@@ -1,29 +1,37 @@
 import * as React from "react"
-import { forwardRef } from "react"
+import { forwardRef, HTMLAttributes } from "react"
 import logoDefaultLarge from "../../../assets/logo/logo-default-large.svg"
 import logoDefaultSmall from "../../../assets/logo/logo-default-small.svg"
 import logoInverseLarge from "../../../assets/logo/logo-inverse-large.svg"
 import logoInverseSmall from "../../../assets/logo/logo-inverse-small.svg"
 import { useColorMode } from "../../../lib/EinrideProvider/EinrideProvider"
 
-export interface LogoProps {
+export interface LogoProps extends HTMLAttributes<HTMLImageElement> {
   size?: "small" | "large"
   variant?: "default" | "inverse"
 }
 
 export const Logo = forwardRef<HTMLImageElement, LogoProps>(
-  ({ size = "small", variant }, ref) => {
+  ({ size = "small", variant, ...props }, ref) => {
     const colorMode = useColorMode()
     const logoDefault = () => {
       if (size === "small")
-        return <img src={logoDefaultSmall} alt="Einride Logo" ref={ref} />
-      return <img src={logoDefaultLarge} alt="Einride Logo" ref={ref} />
+        return (
+          <img src={logoDefaultSmall} alt="Einride Logo" ref={ref} {...props} />
+        )
+      return (
+        <img src={logoDefaultLarge} alt="Einride Logo" ref={ref} {...props} />
+      )
     }
 
     const logoInverse = () => {
       if (size === "small")
-        return <img src={logoInverseSmall} alt="Einride Logo" ref={ref} />
-      return <img src={logoInverseLarge} alt="Einride Logo" ref={ref} />
+        return (
+          <img src={logoInverseSmall} alt="Einride Logo" ref={ref} {...props} />
+        )
+      return (
+        <img src={logoInverseLarge} alt="Einride Logo" ref={ref} {...props} />
+      )
     }
 
     if (variant === "inverse" || (!variant && colorMode === "dark")) {
