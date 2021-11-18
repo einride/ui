aba1b40ae29ee8058d0eb80b46b421f1b2df8566
Filip Tammergård <filip@tammergard.se>
2021-11-18 10:35:54 +0100

    chore: use default release branch config

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  branches: ["main"],
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
   plugins: [
     [
       "@semantic-release/commit-analyzer",
