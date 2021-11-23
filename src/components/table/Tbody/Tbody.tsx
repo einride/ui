0aafec6d7aa14cc5b5cba011b166c3618402441e
Filip Tammergård <filip@tammergard.se>
2021-11-23 15:45:06 +0100

    feat!: add table component with all composable components
    Rename current table component to ComposedTable

---
+++ src/components/table/Tbody/Tbody.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled"
+import { forwardRef, HTMLAttributes, ReactNode } from "react"
+
+interface TbodyProps extends HTMLAttributes<HTMLTableSectionElement> {
+  children: ReactNode
+}
+
+export const Tbody = forwardRef<HTMLTableSectionElement, TbodyProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <StyledTbody {...props} ref={ref}>
+        {children}
+      </StyledTbody>
+    )
+  },
+)
+
+const StyledTbody = styled.tbody``
---
+++ src/components/table/Thead/Thead.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+import { forwardRef, HTMLAttributes, ReactNode } from "react"
+
+export interface TheadProps extends HTMLAttributes<HTMLTableSectionElement> {
+  children: ReactNode
+}
+
+export const Thead = forwardRef<HTMLTableSectionElement, TheadProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <StyledThead {...props} ref={ref}>
+        {children}
+      </StyledThead>
+    )
+  },
+)
+
+const StyledThead = styled.thead`
+  tr {
+    box-shadow: unset;
+  }
+`
