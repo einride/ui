01f3b24846fca5210203aeb4af05ff2ad39decc6
Filip Tammergård <filip@tammergard.se>
2021-11-26 11:28:50 +0100

    chore: add some custom properties

--- src/lib/GlobalStyles/GlobalStyles.tsx
+++ src/lib/GlobalStyles/GlobalStyles.tsx
@@ -9,6 +9,21 @@ export const GlobalStyles = () => {
         html {
           font-family: ${theme.fonts.body};
         }
+
+        body {
+          --einride-ui-grid-columns: 4;
+          --einride-ui-grid-gap: ${2 * theme.spacer}px;
+
+          ${theme.breakpoint.medium} {
+            --einride-ui-grid-columns: 8;
+            --einride-ui-grid-gap: ${3 * theme.spacer}px;
+          }
+
+          ${theme.breakpoint.large} {
+            --einride-ui-grid-columns: 12;
+            --einride-ui-grid-gap: ${3 * theme.spacer}px;
+          }
+        }
       `}
     />
   )
