a0c25318603ed0d2fd1ec8b17d7d6315b1a84819
Filip Tammergård <filip@tammergard.se>
2022-01-19 13:41:03 +0100

    chore: move white and black primitives into greys object for consistency

--- src/primitives/color.ts
+++ src/primitives/color.ts
@@ -1,41 +1,41 @@
 export const color = {
   green: {
-    dark: "#00A500",
-    default: "#1FCF01",
     light: "#99FB66",
+    default: "#1FCF01",
+    dark: "#00A500",
   },
   blue: {
-    dark: "#003DAD",
-    default: "#3C91F1",
     light: "#9ECEFF",
+    default: "#3C91F1",
+    dark: "#003DAD",
   },
   violet: {
-    dark: "#2B1FCA",
-    default: "#6664E2",
     light: "#B1B0F5",
+    default: "#6664E2",
+    dark: "#2B1FCA",
   },
   red: {
-    dark: "#D0190C",
-    default: "#ED2A25",
     light: "#FD6262",
+    default: "#ED2A25",
+    dark: "#D0190C",
   },
   orange: {
-    dark: "#E65000",
-    default: "#EC6D2D",
     light: "#F79874",
+    default: "#EC6D2D",
+    dark: "#E65000",
   },
   yellow: {
-    dark: "#B78600",
-    default: "#FFC043",
     light: "#FFE59D",
+    default: "#FFC043",
+    dark: "#B78600",
   },
-  black: "#121212",
-  white: "#ffffff",
-  greyscale: {
+  greys: {
+    white: "#ffffff",
     grey20: "#F5F5F5",
     grey40: "#EBEBEB",
     grey50: "#DADADA",
     grey80: "#7A7A7A",
     grey100: "#1E1E1F",
+    black: "#121212",
   },
 }
