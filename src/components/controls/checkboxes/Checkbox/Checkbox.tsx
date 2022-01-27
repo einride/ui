3640819177a2265d3c9547d5c21730fabf71ff61
Filip Tammergård <filip@tammergard.se>
2022-01-27 19:47:48 +0100

    chore!: require label for checkboxes and use children instead of prop

--- src/components/controls/checkboxes/Checkbox/Checkbox.tsx
+++ src/components/controls/checkboxes/Checkbox/Checkbox.tsx
@@ -1,8 +1,45 @@
 import styled from "@emotion/styled"
 import * as React from "react"
-import { ChangeEvent, InputHTMLAttributes } from "react"
+import {
+  ChangeEvent,
+  CSSProperties,
+  InputHTMLAttributes,
+  ReactNode,
+} from "react"
 import checkmark from "../../../../assets/icons/checkmark.svg"
 import dash from "../../../../assets/icons/dash.svg"
+
+export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  children: ReactNode
+  labelStyles?: CSSProperties
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Checkbox = ({
+  children,
+  labelStyles,
+  ...props
+}: CheckboxProps) => {
+  return (
+    <StyledLabel style={labelStyles}>
+      <StyledInput {...props} />
+      {children}
+    </StyledLabel>
+  )
+}
+
+const StyledLabel = styled.label`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+  margin: 12px 16px;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.content.primary};
+
+  &:focus-within {
+    text-decoration: underline;
+  }
+`
 
 const StyledInput = styled.input`
   appearance: none;
@@ -11,7 +48,7 @@ const StyledInput = styled.input`
   border-radius: 2px;
   border: 2px solid ${({ theme }) => theme.colors.border.primary};
   background: ${({ theme }) => theme.colors.background.primary};
-  margin: 12px;
+  margin-right: ${({ theme }) => 2 * theme.spacer}px;
 
   &:hover {
     border-color: ${({ theme }) => theme.colors.border.selected};
@@ -41,12 +78,3 @@ const StyledInput = styled.input`
     background-color: ${({ theme }) => theme.colors.background.secondary};
   }
 `
-
-export interface DefaultCheckboxProps
-  extends InputHTMLAttributes<HTMLInputElement> {
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
-}
-
-export const DefaultCheckbox = ({ ...props }: DefaultCheckboxProps) => {
-  return <StyledInput type="checkbox" {...props} />
-}
