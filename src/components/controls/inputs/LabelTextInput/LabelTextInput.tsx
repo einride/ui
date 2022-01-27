af01370973c6d69648011ba8dccbe53ba45388a1
Filip Tammergård <filip@tammergard.se>
2022-01-27 20:49:56 +0100

    chore: base LabelTextInput on BaseInput

--- src/components/controls/inputs/LabelTextInput/LabelTextInput.tsx
+++ src/components/controls/inputs/LabelTextInput/LabelTextInput.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import * as React from "react"
 import { ChangeEvent, FocusEvent, InputHTMLAttributes, ReactNode } from "react"
 import { Theme } from "../../../../lib/theme/theme"
-import { TextInput } from "../TextInput/TextInput"
+import { BaseInput } from "../BaseInput/BaseInput"
 
 export interface LabelTextInputProps
   extends InputHTMLAttributes<HTMLInputElement> {
@@ -20,25 +20,22 @@ export const LabelTextInput = ({
   label,
   required,
   status,
+  statusMessage,
   ...props
 }: LabelTextInputProps) => {
   return (
     <StyledLabel status={status}>
       {label} {required && " (required)"}
-      <TextInput status={status} {...props} />
+      <Wrapper status={status}>
+        <StyledBaseInput
+          icon={getStatusIcon(status)}
+          status={status}
+          {...props}
+        />
+        {status && <StyledMessage>{statusMessage}</StyledMessage>}
+      </Wrapper>
     </StyledLabel>
   )
-}
-
-const getColor = (theme: Theme, status?: Status) => {
-  switch (status) {
-    case "success":
-      return theme.colors.content.positive
-    case "fail":
-      return theme.colors.content.negative
-    default:
-      return theme.colors.content.secondary
-  }
 }
 
 const StyledLabel = styled.label<{ status?: Status }>`
@@ -52,4 +49,54 @@ const StyledLabel = styled.label<{ status?: Status }>`
   }
 `
 
+const Wrapper = styled.div<{ status?: Status }>`
+  color: ${({ theme, status }) => getColor(theme, status)};
+`
+
+const StyledBaseInput = styled(BaseInput)<{ status?: Status }>`
+  background: ${({ theme, status }) => getBackgroundColor(theme, status)};
+  &:hover:not(:disabled) {
+    background: ${({ theme, status }) => getBackgroundColor(theme, status)};
+  }
+`
+
+const StyledMessage = styled.div`
+  color: ${({ theme }) => theme.colors.content.secondary};
+  margin: 3px 0 5px;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`
+
 type Status = "success" | "fail"
+
+const getBackgroundColor = (theme: Theme, status?: Status) => {
+  switch (status) {
+    case "success":
+      return theme.colors.background.positive
+    case "fail":
+      return theme.colors.background.negative
+    default:
+      return theme.colors.background.secondary
+  }
+}
+
+const getColor = (theme: Theme, status?: Status) => {
+  switch (status) {
+    case "success":
+      return theme.colors.content.positive
+    case "fail":
+      return theme.colors.content.negative
+    default:
+      return theme.colors.content.secondary
+  }
+}
+
+const getStatusIcon = (status?: Status) => {
+  switch (status) {
+    case "success":
+      return "✓"
+    case "fail":
+      return "􀇿︎"
+    default:
+      return ""
+  }
+}
