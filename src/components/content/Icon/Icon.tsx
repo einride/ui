bbb193932913a3bce14348615896025be06fa1ed
Filip Tammergård <filip@tammergard.se>
2022-02-11 16:12:03 +0100

    fix: add refs to forwardReffed componenets

--- src/components/content/Icon/Icon.tsx
+++ src/components/content/Icon/Icon.tsx
@@ -5,95 +5,95 @@ export interface IconProps extends HTMLAttributes<HTMLSpanElement> {
 }
 
 export const Icon = forwardRef<HTMLSpanElement, IconProps>(
-  ({ name, ...props }: IconProps) => {
+  ({ name, ...props }, ref) => {
     switch (name) {
       case "checkmark":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#10003;
           </span>
         )
       case "warning":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#1049087;
           </span>
         )
       case "chevronDown":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8964;
           </span>
         )
       case "chevronUp":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8963;
           </span>
         )
       case "chevronRight":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#707;
           </span>
         )
       case "chevronLeft":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#706;
           </span>
         )
       case "xMark":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#10060;
           </span>
         )
       case "arrowUp":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8593;
           </span>
         )
       case "arrowDown":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8595;
           </span>
         )
       case "arrowRight":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8594;
           </span>
         )
       case "arrowLeft":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#8592;
           </span>
         )
       case "arrowUpCircle":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#1048694;
           </span>
         )
       case "arrowDownCircle":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#1048696;
           </span>
         )
       case "bolt":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#128498;
           </span>
         )
       case "loupe":
         return (
-          <span aria-hidden="true" {...props}>
+          <span aria-hidden="true" {...props} ref={ref}>
             &#1049259;
           </span>
         )
