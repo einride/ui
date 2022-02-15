c2f9a65807d8b357aaf18fbc906033b10d6a1759
Filip Tammergård <filip@tammergard.se>
2022-02-15 11:16:45 +0100

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["plugin:@einride/default", "plugin:storybook/recommended"],
   rules: {
+    "jest/no-deprecated-functions": "off",
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
   },
 }
