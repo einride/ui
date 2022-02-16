c36b41e6a4ea6fa70fdb67d4d07450815f277a7b
Filip Tammergård <filip@tammergard.se>
2022-02-16 10:50:03 +0100

    feat: make logo adhere to color mode

--- src/components/content/Logo/Logo.tsx
+++ src/components/content/Logo/Logo.tsx
@@ -1,32 +1,14 @@
 import { forwardRef, SVGAttributes } from "react"
-import { useColorMode } from "../../../lib/EinrideProvider/EinrideProvider"
-import { LogoDefaultLarge } from "./variants/LogoDefaultLarge"
-import { LogoDefaultSmall } from "./variants/LogoDefaultSmall"
-import { LogoInverseLarge } from "./variants/LogoInverseLarge"
-import { LogoInverseSmall } from "./variants/LogoInverseSmall"
+import { LogoLarge } from "./variants/LogoLarge"
+import { LogoSmall } from "./variants/LogoSmall"
 
 export interface LogoProps extends SVGAttributes<SVGSVGElement> {
   size?: "small" | "large"
-  variant?: "default" | "inverse"
 }
 
 export const Logo = forwardRef<SVGSVGElement, LogoProps>(
-  ({ size = "small", variant, ...props }, ref) => {
-    const colorMode = useColorMode()
-    const logoDefault = () => {
-      if (size === "small") return <LogoDefaultSmall ref={ref} {...props} />
-      return <LogoDefaultLarge ref={ref} {...props} />
-    }
-
-    const logoInverse = () => {
-      if (size === "small") return <LogoInverseSmall ref={ref} {...props} />
-      return <LogoInverseLarge ref={ref} {...props} />
-    }
-
-    if (variant === "inverse" || (!variant && colorMode === "dark")) {
-      return logoInverse()
-    }
-
-    return logoDefault()
+  ({ size = "small", ...props }, ref) => {
+    if (size === "small") return <LogoSmall ref={ref} {...props} />
+    return <LogoLarge ref={ref} {...props} />
   },
 )
