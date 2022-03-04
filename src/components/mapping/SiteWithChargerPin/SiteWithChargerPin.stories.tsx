5f41b5378d1e0766a31d24f7b0299450834e354f
Jens Filipsson <jensfilipsson@gmail.com>
2022-03-04 14:02:40 +0100

    feat: add site with charger pin component

---
+++ src/components/mapping/SiteWithChargerPin/SiteWithChargerPin.stories.tsx
@@ -0,0 +1,19 @@
+import { Story } from "@storybook/react/types-6-0"
+import {
+  SiteWithChargerPin,
+  SiteWithChargerPinProps,
+} from "./SiteWithChargerPin"
+
+export default {
+  title: "Mapping/SiteWithChargerPin",
+  component: SiteWithChargerPin,
+}
+
+const Template: Story<SiteWithChargerPinProps> = (args) => (
+  <SiteWithChargerPin {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  size: "md",
+}
