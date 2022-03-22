946e87b759b753d4a4c504b10173d960295e5c84
Filip Tammergård <filip@tammergard.se>
2022-03-22 08:42:44 +0100

    chore: publish to gpr

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   ignores: [
     // ignore dependabot messages
     (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+    // ignore release commits by semantic release
+    (message) => message.includes("chore(release)"),
   ],
 }
