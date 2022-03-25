aca8c21a0da1065f149564659e2241e64f03724b
Filip Tammergård <filip@tammergard.se>
2022-03-25 09:36:01 +0100

    feat: add linear progress component

---
+++ src/components/charts/LinearProgress/LinearProgress.tsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled"
+import { forwardRef, HTMLAttributes } from "react"
+
+export interface LinearProgressProps extends HTMLAttributes<HTMLDivElement> {
+  max?: number
+  min?: number
+  value: number
+}
+
+export const LinearProgress = forwardRef<HTMLDivElement, LinearProgressProps>(
+  ({ max = 100, min = 0, value, ...props }, ref) => {
+    return (
+      <Wrapper
+        role="progressbar"
+        aria-valuemax={max}
+        aria-valuemin={min}
+        aria-valuenow={value}
+        {...props}
+        ref={ref}
+      >
+        <Value max={max} min={min} value={value} />
+      </Wrapper>
+    )
+  },
+)
+
+const Wrapper = styled.div`
+  background: ${({ theme }) => theme.colors.background.tertiary};
+  height: ${({ theme }) => theme.spacer}px;
+  border-radius: ${({ theme }) => theme.spacer}px;
+  position: relative;
+`
+
+const Value = styled.div<{ max: number; min: number; value: number }>`
+  background: ${({ theme }) => theme.colors.content.positive};
+  height: ${({ theme }) => theme.spacer}px;
+  border-radius: ${({ theme }) => theme.spacer}px;
+  width: ${({ max, min, value }) => getWidth(max, min, value)}%;
+`
+
+const getWidth = (max: number, min: number, value: number): number => {
+  if (value > max) return 100
+  if (value < min) return 0
+  return ((value - min) / (max - min)) * 100
+}
