7233175049b004e98abf5d5fa1ec2a0fea77687c
Filip Tammergård <filip@tammergard.se>
2022-03-28 11:14:21 +0200

    feat: make selects hug content instead of using min-width

--- src/components/controls/selects/DefaultDropdownSelect/DefaultDropdownSelect.tsx
+++ src/components/controls/selects/DefaultDropdownSelect/DefaultDropdownSelect.tsx
@@ -63,25 +63,23 @@ const getMessageColor = (status: Status | undefined): ContentColor => {
 const Wrapper = styled.div<{ isFullWidth?: boolean }>`
   position: relative;
   display: inline-block;
-  min-width: 240px;
   ${({ isFullWidth }) => isFullWidth && "width: 100%"};
 `
 
 const StyledSelect = styled.select<{ isFullWidth?: boolean }>`
   font-family: ${({ theme }) => theme.fonts.body};
   font-size: ${({ theme }) => theme.fontSizes.md};
-  min-width: 240px;
   ${({ isFullWidth }) => isFullWidth && "width: 100%"};
   background: ${({ theme }) => theme.colors.background.secondary};
   color: ${({ theme }) => theme.colors.content.primary};
   line-height: 24px;
   display: block;
   padding: 12px 16px;
+  padding-right: ${({ theme }) => 6 * theme.spacer}px;
   border: unset;
   border-radius: 2px;
   cursor: pointer;
   appearance: none;
-  padding-right: 29px;
 
   &:focus {
     box-shadow: 0px 0px 0px 1px ${({ theme }) => theme.colors.border.selected}
@@ -101,7 +99,9 @@ const StyledSelect = styled.select<{ isFullWidth?: boolean }>`
 const StyledIcon = styled(Icon)`
   position: absolute;
   top: ${({ theme }) => 1.5 * theme.spacer}px;
-  right: ${({ theme }) => 1.5 * theme.spacer}px;
+  right: ${({ theme }) => theme.spacer}px;
   pointer-events: none;
   color: ${({ theme }) => theme.colors.content.primary};
+  width: ${({ theme }) => 3 * theme.spacer}px;
+  text-align: center;
 `
