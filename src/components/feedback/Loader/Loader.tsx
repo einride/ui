774f3836340ce131a798169f0bcb467e63529315
Filip Tammergård <filip@tammergard.se>
2022-05-10 09:02:17 +0200

    feat(loader): center horizontally in container

--- src/components/feedback/Loader/Loader.tsx
+++ src/components/feedback/Loader/Loader.tsx
@@ -30,6 +30,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin: auto; // center the loader horizontally
 `
 
 const Line = styled(motion.div)`
