bc243267972b64230993300268a0710f2457de2f
Filip Tammergård <filip@tammergard.se>
2022-05-24 16:45:37 +0200

    chore: bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,12 @@ module.exports = {
   rules: {
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
   },
+  overrides: [
+    {
+      files: ["*.stories.tsx"],
+      rules: {
+        "import/no-default-export": "off", // storybook requires default exports in stories
+      },
+    },
+  ],
 }
