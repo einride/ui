60ce97d12ea508e3061776f6e4eda46ca464c7b7
Filip Tammergård <filip@tammergard.se>
2022-05-27 11:25:09 +0200

    feat: add slider

--- src/components/controls/sliders/Slider/Slider.tsx
+++ src/components/controls/sliders/Slider/Slider.tsx
@@ -1,121 +1,93 @@
 import styled from "@emotion/styled"
-import { ChangeEvent, InputHTMLAttributes, useRef, useState } from "react"
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion"
+import { InputHTMLAttributes, useEffect, useRef, useState } from "react"
 
 export interface SliderProps extends InputHTMLAttributes<HTMLInputElement> {
   value?: number
 }
 
-export const Slider = ({ value = 0, ...props }: SliderProps): JSX.Element => {
+export const Slider = ({ value = 100, ...props }: SliderProps): JSX.Element => {
   const [percentage, setPercentage] = useState(value)
-  const sliderWrapperRef = useRef<HTMLDivElement>(null)
+  const constraintsRef = useRef<HTMLDivElement>(null)
+  const handleRef = useRef<HTMLDivElement>(null)
+  const progressRef = useRef<HTMLDivElement>(null)
+  const handleX = useMotionValue(value)
+  const progress = useTransform(handleX, (x) => x + 48 / 2)
+  const background = useMotionTemplate`linear-gradient(90deg, #374151 ${progress}px, #d1d5db 0)`
 
-  const handleSliderChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setPercentage(parseInt(e.target.value, 10))
+  useEffect(() => {
+    if (!progressRef.current) return
+    const newProgess = percentage / 100
+    const progressBounds = progressRef.current.getBoundingClientRect()
+    handleX.set(newProgess * progressBounds.width)
+  }, [handleX, percentage])
+
+  const handleDrag = (): void => {
+    if (!handleRef.current || !progressRef.current) return
+    const handleBounds = handleRef.current.getBoundingClientRect()
+    const middleOfHandle = handleBounds.x + handleBounds.width / 2
+    const progressBounds = progressRef.current.getBoundingClientRect()
+    const newPercentage =
+      (middleOfHandle - progressBounds.x) / progressBounds.width
+    setPercentage(newPercentage)
   }
 
   return (
-    <Wrapper ref={sliderWrapperRef}>
-      <LeftCircle />
-      <SliderFill percentage={percentage} />
-      <Range
-        type="range"
-        value={percentage}
-        onChange={handleSliderChange}
-        percentage={percentage}
-        {...props}
-      />
-      <ThumbWrapper>
-        <Thumb percentage={percentage} />
-      </ThumbWrapper>
+    <Wrapper
+      style={{ background }}
+      onPointerDown={(event) => {
+        if (!progressRef.current) return
+        const { left, width } = progressRef.current.getBoundingClientRect()
+        const position = event.pageX - left
+        const newProgress = position / width
+        const newValue = newProgress * 100
+        setPercentage(clamp(newValue))
+      }}
+    >
+      <Progress ref={progressRef} />
+      <div ref={constraintsRef}>
+        <Handle
+          drag="x"
+          dragConstraints={constraintsRef}
+          dragElastic={0}
+          dragMomentum={false}
+          onDrag={handleDrag}
+          ref={handleRef}
+          style={{ x: handleX }}
+        />
+      </div>
+      <div style={{ paddingTop: 100 }}>{percentage}</div>
     </Wrapper>
   )
 }
 
-const Wrapper = styled("div", { target: "slider-wrapper" })`
+const clamp = (number: number): number => {
+  return Math.max(0, Math.min(number, 100))
+}
+
+const Wrapper = styled(motion.div)`
   height: ${({ theme }) => 6 * theme.spacer}px;
-  background: ${({ theme }) => theme.colors.background.secondary};
   border-radius: ${({ theme }) => 3 * theme.spacer}px;
   position: relative;
 `
-const LeftCircle = styled.div`
-  height: 100%;
-  width: 20px;
-  background: ${({ theme }) => theme.colors.content.positive};
-  border-top-left-radius: ${({ theme }) => 3 * theme.spacer}px;
-  border-bottom-left-radius: ${({ theme }) => 3 * theme.spacer}px;
-`
 
-const SliderFill = styled.div<{ percentage: number }>`
-  height: 100%;
-  width: ${({ percentage }) => percentage}%;
-  background: ${({ theme }) => theme.colors.content.positive};
-  border-top-left-radius: ${({ theme }) => 3 * theme.spacer}px;
-  border-bottom-left-radius: ${({ theme }) => 3 * theme.spacer}px;
-`
-
-const Range = styled.input<{ percentage: number }>`
+const Progress = styled(motion.div)`
   position: absolute;
-  top: 0;
-  left: 0;
-  appearance: none;
-  background: transparent;
-  cursor: pointer;
-  width: 100%;
   height: ${({ theme }) => 6 * theme.spacer}px;
-
-  &:focus {
-    outline: none;
-  }
-  &::-webkit-slider-runnable-track {
-    height: ${({ theme }) => 6 * theme.spacer}px;
-  }
-  &::-moz-range-track {
-    height: ${({ theme }) => 6 * theme.spacer}px;
-  }
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    box-shadow: none;
-    background: ${({ theme }) => theme.colors.content.positive};
-    border-radius: ${({ theme }) => theme.spacer * 3}px;
-    height: 100%;
-    width: ${({ theme }) => theme.spacer * 6}px;
-  }
-  &::-moz-range-thumb {
-    border: none;
-    background: ${({ theme }) => theme.colors.content.positive};
-    border-radius: ${({ theme }) => theme.spacer * 3}px;
-    height: 100%;
-    width: ${({ theme }) => theme.spacer * 6}px;
-  }
+  left: ${({ theme }) => 3 * theme.spacer}px;
+  right: ${({ theme }) => 3 * theme.spacer}px;
+  border-radius: ${({ theme }) => 3 * theme.spacer}px;
 `
 
-const ThumbWrapper = styled.div`
+const Handle = styled(motion.div)`
   position: absolute;
-  top: 0;
-  left: ${({ theme }) => 2 * theme.spacer}px;
-  width: ${({ theme }) => `calc(100% - ${6 * theme.spacer}px)`};
-  height: 100%;
-  display: flex;
-  align-items: center;
-  pointer-events: none;
-`
-
-const Thumb = styled.div<{ percentage: number }>`
-  width: ${({ theme }) => 2 * theme.spacer}px;
-  height: ${({ theme }) => 2 * theme.spacer}px;
-  border-radius: ${({ theme }) => theme.spacer}px;
-  position: absolute;
-  background: ${({ theme }) => theme.colors.background.primary};
-  left: ${({ percentage }) => `${percentage}%`};
-  pointer-events: none;
-  transition-duration: ${({ theme }) => theme.transitions.easeIn.duration};
-  transition-timing-function: ${({ theme }) => theme.transitions.morph};
-  transition-property: width, height, margin;
-
-  ${Wrapper}:hover & {
-    width: ${({ theme }) => theme.spacer}px;
-    height: ${({ theme }) => 4 * theme.spacer}px;
-    margin-left: ${({ theme }) => 0.5 * theme.spacer}px;
-  }
+  background: ${({ theme }) => theme.colors.content.positive};
+  width: ${({ theme }) => 6 * theme.spacer}px;
+  height: ${({ theme }) => 6 * theme.spacer}px;
+  border-radius: ${({ theme }) => 3 * theme.spacer}px;
 `
