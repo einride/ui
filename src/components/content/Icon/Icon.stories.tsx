a578c777da31c5becd6a765c60e576a0bb86557f
Filip Tammergård <filip@tammergard.se>
2022-06-15 20:22:27 +0200

    chore(icon): show all icons in one story

--- src/components/content/Icon/Icon.stories.tsx
+++ src/components/content/Icon/Icon.stories.tsx
@@ -1,106 +1,142 @@
+import styled from "@emotion/styled"
 import { Story } from "@storybook/react/types-6-0"
+import { PrimaryCard } from "../../cards/PrimaryCard/PrimaryCard"
+import { Caption } from "../../typography/Caption/Caption"
 import { Paragraph } from "../../typography/Paragraph/Paragraph"
 import { Icon, IconProps } from "./Icon"
 
 export default {
   title: "Content/Icon",
   component: Icon,
-  argTypes: {},
-  parameters: {
-    design: {
-      type: "figma",
-      url: "https://www.figma.com/file/YYzIjuZlggbzY16y5gGyWB/UI-Components?node-id=79%3A0",
-    },
-  },
 }
 
-const Template: Story<IconProps> = (args) => (
+const AllTemplate: Story<IconProps> = () => (
+  <Wrapper>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="checkmark" />
+      </Paragraph>
+      <Caption font="mono">checkmark</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="warning" />
+      </Paragraph>
+      <Caption font="mono">warning</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="chevronDown" />
+      </Paragraph>
+      <Caption font="mono">chevronDown</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="chevronUp" />
+      </Paragraph>
+      <Caption font="mono">chevronUp</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="chevronRight" />
+      </Paragraph>
+      <Caption font="mono">chevronRight</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="chevronLeft" />
+      </Paragraph>
+      <Caption font="mono">chevronLeft</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="xMark" />
+      </Paragraph>
+      <Caption font="mono">xMark</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowUp" />
+      </Paragraph>
+      <Caption font="mono">arrowUp</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowDown" />
+      </Paragraph>
+      <Caption font="mono">arrowDown</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowRight" />
+      </Paragraph>
+      <Caption font="mono">arrowRight</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowLeft" />
+      </Paragraph>
+      <Caption font="mono">arrowLeft</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowUpCircle" />
+      </Paragraph>
+      <Caption font="mono">arrowUpCircle</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="arrowDownCircle" />
+      </Paragraph>
+      <Caption font="mono">arrowDownCircle</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="bolt" />
+      </Paragraph>
+      <Caption font="mono">bolt</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="loupe" />
+      </Paragraph>
+      <Caption font="mono">loupe</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="plus" />
+      </Paragraph>
+      <Caption font="mono">plus</Caption>
+    </StyledPrimaryCard>
+    <StyledPrimaryCard background="secondary">
+      <Paragraph>
+        <Icon name="ellipsis" />
+      </Paragraph>
+      <Caption font="mono">ellipsis</Caption>
+    </StyledPrimaryCard>
+  </Wrapper>
+)
+
+export const All = AllTemplate.bind({})
+All.args = {}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: ${({ theme }) => theme.spacer}px;
+`
+
+const StyledPrimaryCard = styled(PrimaryCard)`
+  text-align: center;
+`
+
+const OneTemplate: Story<IconProps> = (args) => (
   <Paragraph>
     <Icon {...args} />
   </Paragraph>
 )
 
-export const Checkmark = Template.bind({})
-Checkmark.args = {
+export const One = OneTemplate.bind({})
+One.args = {
   name: "checkmark",
-}
-
-export const Warning = Template.bind({})
-Warning.args = {
-  name: "warning",
-}
-
-export const ChevronDowm = Template.bind({})
-ChevronDowm.args = {
-  name: "chevronDown",
-}
-
-export const ChevronUp = Template.bind({})
-ChevronUp.args = {
-  name: "chevronUp",
-}
-
-export const ChevronRight = Template.bind({})
-ChevronRight.args = {
-  name: "chevronRight",
-}
-
-export const ChevronLeft = Template.bind({})
-ChevronLeft.args = {
-  name: "chevronLeft",
-}
-
-export const XMark = Template.bind({})
-XMark.args = {
-  name: "xMark",
-}
-
-export const ArrowUp = Template.bind({})
-ArrowUp.args = {
-  name: "arrowUp",
-}
-
-export const ArrowDown = Template.bind({})
-ArrowDown.args = {
-  name: "arrowDown",
-}
-
-export const ArrowRight = Template.bind({})
-ArrowRight.args = {
-  name: "arrowRight",
-}
-
-export const ArrowLeft = Template.bind({})
-ArrowLeft.args = {
-  name: "arrowLeft",
-}
-
-export const ArrowUpCircle = Template.bind({})
-ArrowUpCircle.args = {
-  name: "arrowUpCircle",
-}
-
-export const ArrowDownCircle = Template.bind({})
-ArrowDownCircle.args = {
-  name: "arrowDownCircle",
-}
-
-export const Bolt = Template.bind({})
-Bolt.args = {
-  name: "bolt",
-}
-
-export const Loupe = Template.bind({})
-Loupe.args = {
-  name: "loupe",
-}
-
-export const Plus = Template.bind({})
-Plus.args = {
-  name: "plus",
-}
-
-export const Ellipsis = Template.bind({})
-Ellipsis.args = {
-  name: "ellipsis",
 }
