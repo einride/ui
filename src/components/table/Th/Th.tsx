f5c9959e12be178602f4bcf58708a2028d1f737c
Filip Tammergård <filip@tammergard.se>
2022-06-16 11:12:29 +0200

    feat(th,td): make color and font customizable

--- src/components/table/Th/Th.tsx
+++ src/components/table/Th/Th.tsx
@@ -1,27 +1,39 @@
 import styled from "@emotion/styled"
 import { ElementType, forwardRef, ReactNode, ThHTMLAttributes } from "react"
+import { ContentColor, Font } from "../../../lib/theme/types"
 
 interface ThProps extends ThHTMLAttributes<HTMLTableCellElement> {
   as?: ElementType
   children: ReactNode
+  /**
+   * Default: "primary"
+   */
+  color?: ContentColor
+  /**
+   * Default: "body"
+   */
+  font?: Font
   /**
    * Defines the cells that the header element relates to.
    */
   scope: "col" | "row"
 }
 
-export const Th = forwardRef<HTMLTableCellElement, ThProps>(({ children, ...props }, ref) => {
-  return (
-    <StyledTh {...props} ref={ref}>
-      {children}
-    </StyledTh>
-  )
-})
+export const Th = forwardRef<HTMLTableCellElement, ThProps>(
+  ({ children, color = "primary", font = "body", ...props }, ref) => {
+    return (
+      <StyledTh color={color} font={font} {...props} ref={ref}>
+        {children}
+      </StyledTh>
+    )
+  },
+)
 
-const StyledTh = styled.th`
-  display: table-cell;
-  font-weight: ${({ theme }) => theme.fontWeights.book};
+const StyledTh = styled.th<{ color: ContentColor; font: Font }>`
+  color: ${({ color, theme }) => theme.colors.content[color]};
+  font-family: ${({ font, theme }) => theme.fonts[font]};
   font-size: ${({ theme }) => theme.fontSizes.md};
+  font-weight: ${({ theme }) => theme.fontWeights.book};
   line-height: calc(4 / 3);
   padding-top: ${({ theme }) => 4 * theme.spacer - 3}px;
   padding-right: ${({ theme }) => 2 * theme.spacer}px;
