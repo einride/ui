773c07888d2e695c623a7d837da5eedcde664052
Filip Tammergård <filip@tammergard.se>
2022-06-28 16:58:25 +0200

    fix(sheets): scroll content excluding navbar

--- src/components/views/Sheets/Sheets.tsx
+++ src/components/views/Sheets/Sheets.tsx
@@ -79,18 +79,18 @@ export const Sheets = forwardRef<HTMLDivElement, SheetsProps>(
               {...props}
               ref={mergedRef}
             >
-              <TopNav>
-                <Navigation>
+              <Navigation>
+                <NavigationAction>
                   {navigationAction && <IconButton {...navigationAction} />}
                   {navigationTitle && navigationTitle}
-                </Navigation>
+                </NavigationAction>
                 {isAboveSm && (
                   <MdLgActions>
                     {secondaryAction && <SecondaryButton {...secondaryAction} />}
                     {primaryAction && <PrimaryButton {...primaryAction} />}
                   </MdLgActions>
                 )}
-              </TopNav>
+              </Navigation>
               <Content hasPrimaryAction={!!primaryAction} hasSecondaryAction={!!secondaryAction}>
                 {children}
               </Content>
@@ -127,10 +127,11 @@ const Wrapper = styled(motion.div)<{ size: Size }>`
   bottom: 0;
   left: 0;
   background: ${({ theme }) => theme.colors.background.primaryElevated};
-  border-top-left-radius: ${({ theme }) => 2 * theme.spacer}px;
-  border-top-right-radius: ${({ theme }) => 2 * theme.spacer}px;
+  border-top-left-radius: ${({ theme }) => theme.borderRadii.lg};
+  border-top-right-radius: ${({ theme }) => theme.borderRadii.lg};
+  // prevent navigation bar from overflowing popover
+  overflow: hidden;
   z-index: 2;
-  overflow-y: auto;
 
   @media ${({ theme }) => theme.mediaQueries.md} {
     top: ${({ theme }) => 2 * theme.spacer}px;
@@ -157,7 +158,7 @@ const Wrapper = styled(motion.div)<{ size: Size }>`
   }
 `
 
-const TopNav = styled.nav`
+const Navigation = styled.nav`
   display: flex;
   justify-content: space-between;
   padding: ${({ theme }) => 2 * theme.spacer}px;
@@ -167,7 +168,7 @@ const TopNav = styled.nav`
   z-index: 1; // to cover content with relative positioned elements
 `
 
-const Navigation = styled.div`
+const NavigationAction = styled.div`
   display: flex;
   gap: ${({ theme }) => 2 * theme.spacer}px;
   align-items: center;
@@ -181,11 +182,16 @@ const MdLgActions = styled.div`
 
 const Content = styled.div<{ hasPrimaryAction: boolean; hasSecondaryAction: boolean }>`
   padding-inline: ${({ theme }) => 2 * theme.spacer}px;
-  padding-bottom: ${({ theme }) => 2 * theme.spacer}px;
+  // make sure content is not hidden when actions are added
+  padding-bottom: ${({ hasPrimaryAction, hasSecondaryAction, theme }) =>
+    getPaddingBottom(hasPrimaryAction, hasSecondaryAction, theme)}px;
+  // fix height to enable setting overflow-y
+  height: calc(100% - ${({ theme }) => 10 * theme.spacer}px);
+  // when there's more content than room in the popover, it should scroll and not overlow
+  overflow-y: auto;
 
-  @media ${({ theme }) => theme.mediaQueries.belowMd} {
-    padding-bottom: ${({ hasPrimaryAction, hasSecondaryAction, theme }) =>
-      getPaddingBottom(hasPrimaryAction, hasSecondaryAction, theme)}px;
+  @media ${({ theme }) => theme.mediaQueries.md} {
+    padding-bottom: ${({ theme }) => 2 * theme.spacer}px;
   }
 `
 
