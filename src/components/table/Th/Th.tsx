5e1c2cb9e50d621632c5a299c084d3d0961c4155
Filip Tammergård <filip@tammergard.se>
2022-07-05 15:08:51 +0200

    fix(th, td): set height explicitly

--- src/components/table/Th/Th.tsx
+++ src/components/table/Th/Th.tsx
@@ -35,9 +35,8 @@ const StyledTh = styled.th<{ color: ContentColor; font: Font }>`
   font-size: ${({ theme }) => theme.fontSizes.md};
   font-weight: ${({ theme }) => theme.fontWeights.book};
   line-height: calc(4 / 3);
-  padding-top: ${({ theme }) => 4 * theme.spacer - 3}px;
   padding-right: ${({ theme }) => 2 * theme.spacer}px;
-  padding-bottom: ${({ theme }) => 3 * theme.spacer + 3}px;
   padding-left: ${({ theme }) => 2 * theme.spacer}px;
+  height: ${({ theme }) => 10 * theme.spacer}px;
   text-align: left;
 `
