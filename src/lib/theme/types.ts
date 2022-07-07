166f01c920790100873ec419163c40d018ced69d
Filip Tammergård <filip@tammergard.se>
2022-07-07 14:22:54 +0200

    feat(types): add reverse content and background color

--- src/lib/theme/types.ts
+++ src/lib/theme/types.ts
@@ -7,10 +7,17 @@ export type BackgroundColor =
   | "positive"
   | "negative"
   | "focus"
+  | "reverse"
 
 export type BorderColor = "primary" | "selected"
 
-export type ContentColor = "primary" | "secondary" | "tertiary" | "positive" | "negative"
+export type ContentColor =
+  | "primary"
+  | "secondary"
+  | "tertiary"
+  | "positive"
+  | "negative"
+  | "reverse"
 
 export type Font = "heading" | "body" | "mono"
 
