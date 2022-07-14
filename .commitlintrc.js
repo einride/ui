a5246fc387368fef3992ff11f1f3ee22d2265a63
Filip Tammergård <filip@tammergard.se>
2022-07-14 08:46:09 +0200

    chore: remove commitlint ignore

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [
-    // ignore dependabot messages
-    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
-  ],
 }
