fe3c75f37e30d5e4301d4fc5c07ddbc85b4951be
Filip Tammergård <filip@tammergard.se>
2022-08-08 09:12:04 +0200

    chore: use icon name in icon button

--- src/components/controls/buttons/IconButton/IconButton.stories.tsx
+++ src/components/controls/buttons/IconButton/IconButton.stories.tsx
@@ -1,5 +1,4 @@
 import { Story } from "@storybook/react/types-6-0"
-import { Icon } from "../../../content/Icon/Icon"
 import { IconButton, IconButtonProps } from "./IconButton"
 
 export default {
@@ -20,5 +19,5 @@ Default.args = {}
 export const AddButton = Template.bind({})
 AddButton.args = {
   "aria-label": "Add vehicle",
-  icon: <Icon name="plus" />,
+  icon: "plus",
 }
