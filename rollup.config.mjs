0c8ce8a65624e9a2f95b295516e284f85ba0c68c
Filip Tammergård <filip@tammergard.se>
2022-08-16 16:30:35 +0200

    feat: bundle with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import typescript from "@rollup/plugin-typescript"
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  external: [
+    "@einride/hooks",
+    "@emotion/react",
+    "@emotion/styled",
+    "@mantine/hooks",
+    "framer-motion",
+    "lodash.merge",
+    "react",
+    "react/jsx-runtime",
+  ],
+  input: "src/main.ts",
+  output: {
+    dir: "dist",
+    format: "esm",
+  },
+  plugins: [typescript({ tsconfig: "tsconfig.build.json" })],
+}
