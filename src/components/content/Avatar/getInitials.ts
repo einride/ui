d0a5f32e2f418a325c68d0f5256b4b7cf584207f
Filip Tammergård <filip@tammergard.se>
2022-09-22 15:42:44 +0200

    chore: move getInitials function to separate file

---
+++ src/components/content/Avatar/getInitials.ts
@@ -0,0 +1,10 @@
+export const getInitials = (name: string | undefined): string | null => {
+  if (!name) return null
+
+  const parts = name.split(" ")
+
+  if (parts.length === 1) return parts[0].charAt(0)
+  if (parts.length >= 2) return `${parts[0].charAt(0)}${parts[parts.length - 1].charAt(0)}`
+
+  return null
+}
