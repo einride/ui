8e8275f92f9cb93250a13f7106152c53ffc1ac99
Filip Tammergård <filip@tammergard.se>
2022-09-22 15:44:09 +0200

    feat: make initial upper-case even if name is lower-case

--- src/components/content/Avatar/getInitials.ts
+++ src/components/content/Avatar/getInitials.ts
@@ -3,8 +3,9 @@ export const getInitials = (name: string | undefined): string | null => {
 
   const parts = name.split(" ")
 
-  if (parts.length === 1) return parts[0].charAt(0)
-  if (parts.length >= 2) return `${parts[0].charAt(0)}${parts[parts.length - 1].charAt(0)}`
+  if (parts.length === 1) return parts[0].charAt(0).toUpperCase()
+  if (parts.length >= 2)
+    return `${parts[0].charAt(0)}${parts[parts.length - 1].charAt(0)}`.toUpperCase()
 
   return null
 }
