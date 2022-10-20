81e0d15dcc9e6628277271b41c2e38e62e885756
Filip Tammergård <filip@tammergard.se>
2022-10-20 15:17:19 +0200

    feat(SearchInput): support uncontrolled input

--- src/components/controls/inputs/SearchInput/SearchInput.stories.tsx
+++ src/components/controls/inputs/SearchInput/SearchInput.stories.tsx
@@ -1,5 +1,7 @@
-import { Story } from "@storybook/react/types-6-0"
-import { SearchInput, SearchInputProps } from "./SearchInput"
+import { expect } from "@storybook/jest"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
+import { SearchInput } from "./SearchInput"
 
 export default {
   title: "Controls/Inputs/SearchInput",
@@ -9,11 +11,35 @@ export default {
       control: "boolean",
     },
   },
-}
+} as ComponentMeta<typeof SearchInput>
 
-const Template: Story<SearchInputProps> = (args) => <SearchInput {...args} />
+const Template: ComponentStory<typeof SearchInput> = (args) => <SearchInput {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
+  "aria-label": "Search for something fun",
   placeholder: "Search...",
+}
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Search for something fun")
+}
+
+export const ClearButton = Template.bind({})
+ClearButton.args = {
+  ...Default.args,
+}
+ClearButton.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Search for something fun")
+  await userEvent.type(input, "I'm searching for something fun!", { delay: 10 })
+  await expect(input).toHaveValue("I'm searching for something fun!")
+  const clearButton = canvas.getByRole("button")
+  await expect(clearButton).toBeInTheDocument()
+  await expect(clearButton).toHaveAccessibleName("Clear input")
+  await userEvent.click(clearButton)
+  await expect(input).toHaveValue("")
+  await expect(clearButton).not.toBeInTheDocument()
 }
