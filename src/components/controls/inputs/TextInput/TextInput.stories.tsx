b2f2738200c694fb9feaaa5a72ad51cdbe7a1b6a
Filip Tammergård <filip@tammergard.se>
2022-10-20 13:56:34 +0200

    chore: add storybook test-runner

--- src/components/controls/inputs/TextInput/TextInput.stories.tsx
+++ src/components/controls/inputs/TextInput/TextInput.stories.tsx
@@ -1,5 +1,7 @@
-import { Story } from "@storybook/react/types-6-0"
-import { TextInput, TextInputProps } from "./TextInput"
+import { expect } from "@storybook/jest"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
+import { TextInput } from "./TextInput"
 
 export default {
   title: "Controls/Inputs/TextInput",
@@ -9,31 +11,72 @@ export default {
       control: "boolean",
     },
   },
-}
+} as ComponentMeta<typeof TextInput>
 
-const Template: Story<TextInputProps> = (args) => <TextInput {...args} />
+const Template: ComponentStory<typeof TextInput> = (args) => <TextInput {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
+  label: "Label",
+}
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Label")
+  await expect(input).not.toHaveAccessibleDescription()
+  await expect(input).not.toHaveErrorMessage()
+}
+
+export const WithoutLabel = Template.bind({})
+WithoutLabel.args = {
   "aria-label": "Label",
   placeholder: "Placeholder...",
 }
-
-export const Label = Template.bind({})
-Label.args = {
-  label: "Label",
+WithoutLabel.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Label")
+  await expect(input).not.toHaveAccessibleDescription()
+  await expect(input).not.toHaveErrorMessage()
 }
 
-export const Positive = Template.bind({})
-Positive.args = {
-  ...Label.args,
-  message: "Confirmation message.",
+export const Message = Template.bind({})
+Message.args = {
+  ...Default.args,
+  message: "Message.",
+}
+Message.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Label")
+  await expect(input).toHaveAccessibleDescription("Message.")
+  await expect(input).not.toHaveErrorMessage()
+}
+
+export const SuccessMessage = Template.bind({})
+SuccessMessage.args = {
+  ...Default.args,
+  message: "Success message.",
   status: "success",
 }
+SuccessMessage.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Label")
+  await expect(input).toHaveAccessibleDescription("Success message.")
+  await expect(input).not.toHaveErrorMessage()
+}
 
-export const Negative = Template.bind({})
-Negative.args = {
-  ...Label.args,
+export const ErrorMessage = Template.bind({})
+ErrorMessage.args = {
+  ...Default.args,
   message: "Error message.",
   status: "fail",
+}
+ErrorMessage.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = canvas.getByRole("textbox")
+  await expect(input).toHaveAccessibleName("Label")
+  await expect(input).not.toHaveAccessibleDescription()
+  await expect(input).toHaveErrorMessage("Error message.")
 }
