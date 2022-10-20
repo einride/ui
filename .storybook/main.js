cc2e6c7c1cb5bd29f637499569797649ce526613
Filip Tammergård <filip@tammergard.se>
2022-10-20 13:56:34 +0200

    chore: add storybook test-runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,16 @@ module.exports = {
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
     "@storybook/addon-links",
     "storybook-dark-mode",
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-vite",
+  },
+  features: {
+    interactionsDebugger: true,
   },
   staticDirs: ["../public"],
 }
