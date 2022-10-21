513584b0d2d3b29371377c7789e7f989cdd670ac
Filip Tammergård <filip@tammergard.se>
2022-10-21 15:17:18 +0200

    chore: replace vite.config.js with vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 /// <reference types="vitest" />
-
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vite"
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
     environment: "happy-dom",
