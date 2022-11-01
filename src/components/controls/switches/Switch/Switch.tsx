fabfb995e1f0e1321b16fcd906df231ef32b7fb9
Filip Tammergård <filip@tammergard.se>
2022-11-01 15:22:50 +0100

    fix(Switch): correct switch animation

--- src/components/controls/switches/Switch/Switch.tsx
+++ src/components/controls/switches/Switch/Switch.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/destructuring-assignment */
 import styled from "@emotion/styled"
 import * as SwitchPrimitive from "@radix-ui/react-switch"
 import { ComponentPropsWithoutRef, ReactNode, useId } from "react"
@@ -34,6 +33,7 @@ export const Switch = (props: SwitchProps): JSX.Element => {
   const id = useId()
   return (
     <Group alignItems="center" gap="sm">
+      {/* eslint-disable-next-line react/destructuring-assignment */}
       {"label" in props && <StyledLabel htmlFor={id}>{props.label}</StyledLabel>}
       <Root {...props}>
         <SwitchPrimitive.Thumb asChild id={id}>
@@ -57,17 +57,18 @@ const Root = styled(SwitchPrimitive.Root)`
   transition-duration: ${({ theme }) => theme.transitions.easeIn.duration};
   transition-timing-function: ${({ theme }) => theme.transitions.easeIn.timingFunction};
 
-  &[data-state="checked"] {
-    background: ${({ theme }) => theme.colors.content.positive};
+  :is(:hover, :focus-visible) [data-anatomy="inner-thumb"] {
+    transform: scale(1.5);
   }
 
-  :hover [data-anatomy="inner-thumb"] {
-    transform: scale(1.5);
+  :hover,
+  :focus {
+    background: ${({ theme }) => theme.colors.background.tertiary};
   }
 
   :focus-visible {
     outline: none;
-    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.border.selected};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.border.selected};
   }
 `
 
@@ -78,15 +79,15 @@ const OuterThumb = styled.div`
   background: ${({ theme }) => theme.colors.background.primaryInverted};
   border-radius: ${({ theme }) => theme.borderRadii.full};
   display: flex;
-  justify-content: center;
+  justify-content: end;
   align-items: center;
-  transition-property: background transform;
+  transition-property: background, inline-size;
   transition-duration: ${({ theme }) => theme.transitions.easeIn.duration};
   transition-timing-function: ${({ theme }) => theme.transitions.easeIn.timingFunction};
 
   &[data-state="checked"] {
-    transform: translateX(${({ theme }) => 6 * theme.spacingBase}rem);
-    background: none;
+    inline-size: ${({ theme }) => 12 * theme.spacingBase}rem;
+    background: ${({ theme }) => theme.colors.content.positive};
   }
 `
 
@@ -95,6 +96,7 @@ const InnerThumb = styled.div`
   inline-size: ${({ theme }) => 2 * theme.spacingBase}rem;
   background: ${({ theme }) => theme.colors.background.primary};
   border-radius: ${({ theme }) => theme.borderRadii.full};
+  margin-inline-end: ${({ theme }) => theme.spacing.sm};
   transition-property: transform;
   transition-duration: ${({ theme }) => theme.transitions.easeIn.duration};
   transition-timing-function: ${({ theme }) => theme.transitions.easeIn.timingFunction};
