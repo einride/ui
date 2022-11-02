321ee0d2605a56bd616442d2594b772cc74fa3c6
Filip Tammergård <filip@tammergard.se>
2022-11-02 16:05:32 +0100

    chore: add interactive tests

--- src/components/format/Weight/Weight.stories.tsx
+++ src/components/format/Weight/Weight.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { Weight } from "./Weight"
 
 export default {
@@ -14,10 +16,32 @@ Metric.args = {
   locales: "en-US",
   unit: "metric",
 }
+Metric.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const weight = canvas.getByText("123,456.8 kg")
+  expect(weight).toBeInTheDocument()
+}
 
 export const Imperial = Template.bind({})
 Imperial.args = {
   kilograms: 123456.789,
   locales: "en-US",
   unit: "imperial",
+}
+Imperial.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const weight = canvas.getByText("272,175.6 lb")
+  expect(weight).toBeInTheDocument()
+}
+
+export const ImperialInSwedish = Template.bind({})
+ImperialInSwedish.args = {
+  kilograms: 123456.789,
+  locales: "sv-SE",
+  unit: "imperial",
+}
+ImperialInSwedish.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const unit = canvas.getByText("272 175,6 pund")
+  expect(unit).toBeInTheDocument()
 }
--- src/components/format/WeightUnit/WeightUnit.stories.tsx
+++ src/components/format/WeightUnit/WeightUnit.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { WeightUnit } from "./WeightUnit"
 
 export default {
@@ -13,9 +15,30 @@ Metric.args = {
   locales: "en-US",
   unitSystem: "metric",
 }
+Metric.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const unit = canvas.getByText("kg")
+  expect(unit).toBeInTheDocument()
+}
 
 export const Imperial = Template.bind({})
 Imperial.args = {
   locales: "en-US",
   unitSystem: "imperial",
+}
+Imperial.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const unit = canvas.getByText("lb")
+  expect(unit).toBeInTheDocument()
+}
+
+export const ImperialInSwedish = Template.bind({})
+ImperialInSwedish.args = {
+  locales: "sv-SE",
+  unitSystem: "imperial",
+}
+ImperialInSwedish.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const unit = canvas.getByText("pund")
+  expect(unit).toBeInTheDocument()
 }
