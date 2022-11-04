9215ae5f5e524b5a9bfe50fee47c3bd5d9f03e6f
Filip Tammergård <filip@tammergard.se>
2022-11-04 16:19:16 +0100

    feat(Slider): improve accessibility

--- src/components/controls/sliders/Slider/Slider.stories.tsx
+++ src/components/controls/sliders/Slider/Slider.stories.tsx
@@ -1,31 +1,68 @@
-import { Story } from "@storybook/react/types-6-0"
+import { expect } from "@storybook/jest"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
 import { useState } from "react"
-import { Slider, SliderProps } from "./Slider"
+import { Slider } from "./Slider"
 
 export default {
   title: "Controls/Sliders/Slider",
   component: Slider,
-}
+} as ComponentMeta<typeof Slider>
 
-const Template: Story<SliderProps> = (args) => <Slider {...args} />
+const Template: ComponentStory<typeof Slider> = (args) => <Slider {...args} />
 
 export const Default = Template.bind({})
 Default.args = {}
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const slider = canvas.getByRole("slider")
+  await expect(slider).toHaveAttribute("aria-valuenow", "0")
+}
 
-const ControlledTemplate: Story<SliderProps> = (args) => {
-  const [value, setValue] = useState(10)
-
-  const handleChange = (newValue: number): void => {
-    setValue(newValue)
-  }
-
-  return <Slider {...args} value={value} onInputChange={handleChange} />
+const ControlledTemplate: ComponentStory<typeof Slider> = (args) => {
+  const [value, setValue] = useState([0])
+  return <Slider {...args} value={value} onValueChange={setValue} />
 }
 
 export const Controlled = ControlledTemplate.bind({})
-Controlled.args = {}
+Controlled.args = {
+  ...Default.args,
+}
+Controlled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const slider = canvas.getByRole("slider")
+  await expect(slider).toHaveAttribute("aria-valuenow", "0")
+}
 
-export const Disabled = ControlledTemplate.bind({})
+export const Keyboard = Template.bind({})
+Keyboard.args = {
+  ...Default.args,
+}
+Keyboard.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const slider = canvas.getByRole("slider")
+  await expect(slider).toHaveAttribute("aria-valuenow", "0")
+  await userEvent.click(slider)
+  await userEvent.keyboard("[ArrowRight]")
+  await userEvent.keyboard("[ArrowRight]")
+  await userEvent.keyboard("[ArrowRight]")
+  await userEvent.keyboard("[ArrowRight]")
+  await userEvent.keyboard("[ArrowRight]")
+  await expect(slider).toHaveAttribute("aria-valuenow", "5")
+}
+
+export const Disabled = Template.bind({})
 Disabled.args = {
+  ...Default.args,
+  defaultValue: [10],
   disabled: true,
+}
+Disabled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const slider = canvas.getByRole("slider")
+  await expect(slider).toHaveAttribute("aria-valuenow", "10")
+  await expect(slider).toHaveAttribute("data-disabled", "")
+  await userEvent.click(slider)
+  await userEvent.keyboard("[ArrowRight]")
+  await expect(slider).toHaveAttribute("aria-valuenow", "10")
 }
--- src/components/controls/sliders/Slider/Slider.tsx
+++ src/components/controls/sliders/Slider/Slider.tsx
@@ -1,202 +1,137 @@
 import styled from "@emotion/styled"
-import { ChangeEvent, forwardRef, InputHTMLAttributes, useEffect, useState } from "react"
+import * as SliderPrimitive from "@radix-ui/react-slider"
+import { forwardRef } from "react"
 
-export interface SliderProps extends InputHTMLAttributes<HTMLInputElement> {
+interface SliderProps {
+  /** Default slider value when uncontrolled. */
+  defaultValue?: Array<number>
+
+  /** When true, prevents the user from interacting with the slider. */
+  disabled?: boolean
+
   /** Maximum possible value. Default is ´100´. */
   max?: number
 
   /** Minimum possible value. Default is `0`. */
   min?: number
 
-  /** Called each time value changes. */
-  onInputChange?: (value: number) => void
+  /** The name of the slider. */
+  name?: string
 
-  /** Called when user stops dragging slider. */
-  onInputChangeEnd?: (value: number) => void
+  /** Event handler called when the value changes. */
+  onValueChange?: (value: Array<number>) => void
 
-  /** Current value for controlled slider. */
-  value?: number
+  /** Event handler called when the value changes at the end of an interaction. */
+  onValueCommit?: (value: Array<number>) => void
+
+  /** The stepping interval. */
+  step?: number
+
+  /** Controlled slider value. Use together with `onValueChange`. */
+  value?: Array<number>
 }
 
-export const Slider = forwardRef<HTMLInputElement, SliderProps>(
-  ({ max = 100, min = 0, onInputChange, onInputChangeEnd, value = 0, ...props }, ref) => {
-    const [hasFocus, setHasFocus] = useState(false)
-    const [internalValue, setInternalValue] = useState(value)
-
-    useEffect(() => {
-      setInternalValue(value)
-    }, [value])
-
-    const currentValue = (): number => {
-      // make sure we keep our value within the min and max range of the slider
-      if (internalValue <= min) return min
-      if (internalValue >= max) return max
-      return internalValue
-    }
-
-    const calculatedPercentage = (): number => ((currentValue() - min) / (max - min)) * 100
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-      const newValue = parseInt(e.target.value, 10)
-      setInternalValue(newValue)
-      onInputChange?.(newValue)
-    }
-
-    const handleChangeEnd = (): void => {
-      onInputChangeEnd?.(internalValue)
-    }
-
+export const Slider = forwardRef<HTMLSpanElement, SliderProps>(
+  ({ max = 100, min = 0, ...props }, ref) => {
     return (
-      <Wrapper>
-        <LeftFill disabled={!!props.disabled} />
-        <FillWrapper>
-          <Fill disabled={!!props.disabled} percentage={calculatedPercentage()} />
-        </FillWrapper>
-        <Range
-          type="range"
-          max={max}
-          min={min}
-          onChange={handleChange}
-          onMouseUp={handleChangeEnd}
-          onTouchEnd={handleChangeEnd}
-          onFocus={() => setHasFocus(true)}
-          onBlur={() => setHasFocus(false)}
-          value={currentValue()}
-          {...props}
-          ref={ref}
-        />
-        <ThumbWrapper>
-          <Thumb
-            disabled={!!props.disabled}
-            hasFocus={hasFocus}
-            percentage={calculatedPercentage()}
-          />
-        </ThumbWrapper>
-      </Wrapper>
+      <Root max={max} min={min} {...props} ref={ref}>
+        <StartRange data-anatomy="start-range" />
+        <Track>
+          <InnerTrack>
+            <Range />
+          </InnerTrack>
+        </Track>
+        <OuterThumb>
+          <InnerThumb data-anatomy="inner-thumb" />
+        </OuterThumb>
+      </Root>
     )
   },
 )
 
-const Wrapper = styled("div", { target: "wrapper" })`
+const Root = styled(SliderPrimitive.Root)`
+  position: relative;
+  display: flex;
+  cursor: pointer;
+
+  :is(:hover, :focus-visible):not([data-disabled]) [data-anatomy="inner-thumb"] {
+    inline-size: ${({ theme }) => theme.spacingBase}rem;
+    block-size: ${({ theme }) => 4 * theme.spacingBase}rem;
+  }
+
+  &[data-disabled] {
+    cursor: not-allowed;
+
+    [data-anatomy="start-range"] {
+      background: ${({ theme }) => theme.colors.content.tertiary};
+    }
+  }
+`
+
+const StartRange = styled.span`
+  position: absolute;
+  block-size: ${({ theme }) => 6 * theme.spacingBase}rem;
+  inline-size: ${({ theme }) => 3 * theme.spacingBase}rem;
+  background: ${({ theme }) => theme.colors.content.positive};
+  border-start-start-radius: ${({ theme }) => theme.borderRadii.xl};
+  border-end-start-radius: ${({ theme }) => theme.borderRadii.xl};
+`
+
+const Track = styled(SliderPrimitive.Track)`
+  inline-size: 100%;
+  padding-inline: ${({ theme }) => 3 * theme.spacingBase}rem;
   block-size: ${({ theme }) => 6 * theme.spacer}px;
   background: ${({ theme }) => theme.colors.background.secondary};
   border-radius: ${({ theme }) => theme.borderRadii.xl};
+`
+
+const InnerTrack = styled.span`
+  display: block;
+  block-size: 100%;
   position: relative;
 `
 
-const LeftFill = styled.div<{ disabled: boolean }>`
+const Range = styled(SliderPrimitive.Range)`
   position: absolute;
+  inset-inline: 24px;
   block-size: 100%;
-  inline-size: ${({ theme }) => 3 * theme.spacer}px;
-  background: ${({ disabled, theme }) =>
-    disabled ? theme.colors.content.tertiary : theme.colors.content.positive};
-  border-start-start-radius: ${({ theme }) => 3 * theme.spacer}px;
-  border-end-start-radius: ${({ theme }) => 3 * theme.spacer}px;
-`
+  background: ${({ theme }) => theme.colors.content.positive};
 
-const FillWrapper = styled.div`
-  position: absolute;
-  block-size: 100%;
-  inset-block-start: 0;
-  inset-inline: ${({ theme }) => 3 * theme.spacer}px;
-`
-
-const Fill = styled.div<{ disabled: boolean; percentage: number }>`
-  block-size: 100%;
-  inline-size: ${({ percentage }) => percentage}%;
-  background: ${({ disabled, theme }) =>
-    disabled ? theme.colors.content.tertiary : theme.colors.content.positive};
-`
-
-const Range = styled.input<{ disabled?: boolean | undefined }>`
-  position: absolute;
-  inset-block-start: 0;
-  appearance: none;
-  background: transparent;
-  ${({ disabled }) => !disabled && "cursor: pointer"};
-  inline-size: 100%;
-  block-size: 100%;
-  &:focus {
-    outline: none;
-  }
-
-  &::-webkit-slider-runnable-track {
-    block-size: ${({ theme }) => 6 * theme.spacer}px;
-  }
-  &::-moz-range-track {
-    block-size: ${({ theme }) => 6 * theme.spacer}px;
-  }
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    box-shadow: none;
-    background: ${({ disabled, theme }) =>
-      // Since tertiary has an opacity and the same color doesn't exist in backgrounds,
-      // we need to use a gradient with half of the thumb transparent in disabled state.
-      disabled
-        ? `linear-gradient(90deg, transparent 0%, transparent 50%, ${theme.colors.content.tertiary} 50%, ${theme.colors.content.tertiary} 100%)`
-        : theme.colors.content.positive};
-    border-radius: ${({ theme }) => theme.borderRadii.xl};
-    inline-size: ${({ theme }) => 6 * theme.spacer}px;
-    block-size: 100%;
-  }
-  &::-moz-range-thumb {
-    border: none;
-    background: ${({ disabled, theme }) =>
-      // Since tertiary has an opacity and the same color doesn't exist in backgrounds,
-      // we need to use a gradient with half of the thumb transparent in disabled state.
-      disabled
-        ? `linear-gradient(90deg, transparent 0%, transparent 50%, ${theme.colors.content.tertiary} 50%, ${theme.colors.content.tertiary} 100%)`
-        : theme.colors.content.positive};
-    border-radius: ${({ theme }) => theme.borderRadii.xl};
-    inline-size: ${({ theme }) => 6 * theme.spacer}px;
-    block-size: 100%;
+  &[data-disabled] {
+    background: ${({ theme }) => theme.colors.content.tertiary};
   }
 `
 
-const ThumbWrapper = styled.div`
-  position: absolute;
-  inset-block-start: 0;
-  inset-inline-start: ${({ theme }) => 2 * theme.spacer}px;
-  inline-size: ${({ theme }) => `calc(100% - ${6 * theme.spacer}px)`};
-  block-size: 100%;
+const OuterThumb = styled(SliderPrimitive.Thumb)`
+  background: ${({ theme }) => theme.colors.content.positive};
+  block-size: ${({ theme }) => 6 * theme.spacingBase}rem;
+  inline-size: ${({ theme }) => 6 * theme.spacingBase}rem;
+  border-radius: ${({ theme }) => theme.borderRadii.xl};
   display: flex;
+  justify-content: center;
   align-items: center;
-  pointer-events: none;
+
+  &[data-disabled] {
+    background: ${({ theme }) =>
+      `linear-gradient(90deg, transparent 0%, transparent 50%, ${theme.colors.content.tertiary} 50%, ${theme.colors.content.tertiary} 100%)`};
+  }
+
+  :focus-visible {
+    outline: none;
+
+    [data-anatomy="inner-thumb"] {
+      inline-size: ${({ theme }) => theme.spacingBase}rem;
+      block-size: ${({ theme }) => 4 * theme.spacingBase}rem;
+    }
+  }
 `
 
-const Thumb = styled.div<{
-  disabled: boolean
-  hasFocus: boolean
-  percentage: number
-}>`
-  inline-size: ${({ theme }) => 2 * theme.spacer}px;
-  block-size: ${({ theme }) => 2 * theme.spacer}px;
-  border-radius: ${({ theme }) => theme.borderRadii.sm};
-  position: absolute;
+const InnerThumb = styled.span`
+  block-size: ${({ theme }) => 2 * theme.spacingBase}rem;
+  inline-size: ${({ theme }) => 2 * theme.spacingBase}rem;
   background: ${({ theme }) => theme.colors.background.primary};
-  left: ${({ percentage }) => `${percentage}%`};
-  pointer-events: none;
-  transition-duration: ${({ theme }) => theme.transitions.morph.duration};
-  transition-timing-function: ${({ theme }) => theme.transitions.morph.timingFunction};
-  transition-property: width, height, margin;
-
-  ${({ disabled, hasFocus, theme }) =>
-    hasFocus &&
-    !disabled &&
-    `
-    inline-size: ${theme.spacer}px;
-    block-size: ${4 * theme.spacer}px;
-    margin-inline-start: ${0.5 * theme.spacer}px;
-  `};
-
-  ${({ disabled, theme }) =>
-    !disabled &&
-    `
-  ${Wrapper}:hover & {
-    inline-size: ${theme.spacer}px;
-    block-size: ${4 * theme.spacer}px;
-    margin-inline-start: ${0.5 * theme.spacer}px;
-  }
-`}
+  border-radius: ${({ theme }) => theme.borderRadii.full};
+  transition-property: inline-size, block-size;
+  transition-duration: ${({ theme }) => theme.transitions.easeIn.duration};
+  transition-timing-function: ${({ theme }) => theme.transitions.easeIn.timingFunction};
 `
