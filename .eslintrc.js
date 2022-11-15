3925c7d3539575cbd217fcfb8e0dc869d79cbc82
Filip Tammergård <filip@tammergard.se>
2022-11-15 13:27:53 +0100

    chore: add root flag to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["plugin:@einride/default", "plugin:storybook/recommended"],
+  root: true,
   rules: {
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
   },
