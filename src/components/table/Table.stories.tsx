44466ee7df1a7f7719140833233d3a333d474b2b
Filip Tammergård <filip@tammergard.se>
2022-11-17 08:40:01 +0100

    chore(Table): add interaction test

--- src/components/table/Table.stories.tsx
+++ src/components/table/Table.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { Table } from "./Table/Table"
 import { Tbody } from "./Tbody/Tbody"
 import { Td } from "./Td/Td"
@@ -43,3 +45,21 @@ export const Default: ComponentStory<typeof Table> = (args) => (
     </Tbody>
   </Table>
 )
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const table = canvas.getByRole("table")
+  await expect(table).toBeInTheDocument()
+  const columnHeaders = canvas.getAllByRole("columnheader")
+  await expect(columnHeaders).toHaveLength(4)
+  const firstColumnHeader = columnHeaders[0]
+  await expect(firstColumnHeader).toHaveAccessibleName(/country/i)
+  const rowHeaders = canvas.getAllByRole("rowheader")
+  await expect(rowHeaders).toHaveLength(3)
+  const firstRowHeader = rowHeaders[0]
+  await expect(firstRowHeader).toHaveAccessibleName(/sweden/i)
+  const cells = canvas.getAllByRole("cell")
+  await expect(cells).toHaveLength(9)
+  const firstCell = cells[0]
+  await expect(firstCell).toHaveAccessibleName(/stockholm/i)
+}
