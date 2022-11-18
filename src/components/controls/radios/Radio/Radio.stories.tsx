1c463cf7bf4dbb4f9d842cd9034c8f8c60dd53a0
Filip Tammergård <filip@tammergard.se>
2022-11-18 12:18:04 +0100

    chore(Radio): add interaction tests
    Force @testing-library/user-event to a version supporting radio focus behavior.

--- src/components/controls/radios/Radio/Radio.stories.tsx
+++ src/components/controls/radios/Radio/Radio.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
 import { Radio } from "./Radio"
 
 export default {
@@ -17,9 +19,93 @@ export const Default = Template.bind({})
 Default.args = {
   children: "Label",
 }
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const radio = canvas.getByRole("radio")
+  await expect(radio).toHaveAccessibleName(/label/i)
+  await expect(radio).not.toBeChecked()
+  await userEvent.click(radio)
+  await expect(radio).toBeChecked()
+}
 
 export const Controlled = Template.bind({})
 Controlled.args = {
   checked: true,
   children: "Label",
+}
+Controlled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const radio = canvas.getByRole("radio")
+  await expect(radio).toHaveAccessibleName(/label/i)
+  await expect(radio).toBeChecked()
+}
+
+const GroupTemplate: ComponentStory<typeof Radio> = () => {
+  return (
+    <>
+      <Radio name="name">Label 1</Radio>
+      <Radio name="name">Label 2</Radio>
+      <Radio name="name">Label 3</Radio>
+    </>
+  )
+}
+
+export const Group = GroupTemplate.bind({})
+Group.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const radio1 = canvas.getByRole("radio", { name: "Label 1" })
+  const radio2 = canvas.getByRole("radio", { name: "Label 2" })
+  const radio3 = canvas.getByRole("radio", { name: "Label 3" })
+  await expect(radio1).toHaveAccessibleName(/label 1/i)
+  await expect(radio2).toHaveAccessibleName(/label 2/i)
+  await expect(radio3).toHaveAccessibleName(/label 3/i)
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.click(radio1)
+  await expect(radio1).toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.click(radio2)
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.click(radio3)
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).toBeChecked()
+  await userEvent.click(radio3) // ensure you can't uncheck radio button by clicking again
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).toBeChecked()
+}
+
+export const Keyboard = GroupTemplate.bind({})
+Keyboard.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const radio1 = canvas.getByRole("radio", { name: "Label 1" })
+  const radio2 = canvas.getByRole("radio", { name: "Label 2" })
+  const radio3 = canvas.getByRole("radio", { name: "Label 3" })
+  await userEvent.tab()
+  await expect(radio1).toHaveFocus()
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.keyboard("[Space]")
+  await expect(radio1).toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.keyboard("[ArrowDown]")
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).toBeChecked()
+  await expect(radio3).not.toBeChecked()
+  await userEvent.keyboard("[ArrowUp]")
+  await userEvent.keyboard("[ArrowUp]") // ensure radio focus loops
+  await expect(radio1).not.toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).toBeChecked()
+  await userEvent.keyboard("[ArrowDown]")
+  await expect(radio1).toBeChecked()
+  await expect(radio2).not.toBeChecked()
+  await expect(radio3).not.toBeChecked()
 }
