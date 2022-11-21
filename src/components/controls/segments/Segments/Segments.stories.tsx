41b8bad813b721cba82511c53bbf168910b1ccd6
Filip Tammergård <filip@tammergard.se>
2022-11-21 13:46:02 +0100

    fix(Segments): make segments possible to control

--- src/components/controls/segments/Segments/Segments.stories.tsx
+++ src/components/controls/segments/Segments/Segments.stories.tsx
@@ -1,4 +1,7 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
+import { useState } from "react"
 import { Paragraph } from "../../../typography/Paragraph/Paragraph"
 import { Segments } from "./Segments"
 
@@ -9,8 +12,8 @@ export default {
 
 const Template: ComponentStory<typeof Segments> = (args) => <Segments {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Basic = Template.bind({})
+Basic.args = {
   segments: [
     {
       id: "segment-1",
@@ -28,4 +31,113 @@ Default.args = {
       content: <Paragraph>Third segment content.</Paragraph>,
     },
   ],
+}
+Basic.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const tablist = canvas.getByRole("tablist")
+  await expect(tablist).toBeInTheDocument()
+}
+
+export const DefaultValue = Template.bind({})
+DefaultValue.args = {
+  ...Basic.args,
+  defaultValue: 2,
+}
+DefaultValue.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const tab1 = canvas.getByRole("tab", { name: /first segment/i })
+  const tab2 = canvas.getByRole("tab", { name: /second segment/i })
+  const tab3 = canvas.getByRole("tab", { name: /third segment/i })
+  const tab1Content = /first segment content/i
+  const tab2Content = /second segment content/i
+  const tab3Content = /third segment content/i
+  await expect(tab1).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab2).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab3).toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).toBeInTheDocument()
+}
+
+const ControlledTemplate: ComponentStory<typeof Segments> = (args) => {
+  const [value, setValue] = useState(1)
+  return <Segments {...args} value={value} onValueChange={setValue} />
+}
+
+export const Controlled = ControlledTemplate.bind({})
+Controlled.args = {
+  ...Basic.args,
+}
+Controlled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const tablist = canvas.getByRole("tablist")
+  await expect(tablist).toBeInTheDocument()
+}
+
+export const MouseNavigation = Template.bind({})
+MouseNavigation.args = {
+  ...Basic.args,
+}
+MouseNavigation.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const tab1 = canvas.getByRole("tab", { name: /first segment/i })
+  const tab2 = canvas.getByRole("tab", { name: /second segment/i })
+  const tab3 = canvas.getByRole("tab", { name: /third segment/i })
+  const tab1Content = /first segment content/i
+  const tab2Content = /second segment content/i
+  const tab3Content = /third segment content/i
+  await expect(tab1).toHaveAttribute("aria-selected", "true")
+  await expect(tab2).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab3).not.toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).not.toBeInTheDocument()
+  await userEvent.click(tab2)
+  await expect(tab1).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab2).toHaveAttribute("aria-selected", "true")
+  await expect(tab3).not.toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).not.toBeInTheDocument()
+  await userEvent.click(tab3)
+  await expect(tab1).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab2).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab3).toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).toBeInTheDocument()
+}
+
+export const ControlledMouseNavigation = ControlledTemplate.bind({})
+ControlledMouseNavigation.args = {
+  ...Basic.args,
+}
+ControlledMouseNavigation.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const tab1 = canvas.getByRole("tab", { name: /first segment/i })
+  const tab2 = canvas.getByRole("tab", { name: /second segment/i })
+  const tab3 = canvas.getByRole("tab", { name: /third segment/i })
+  const tab1Content = /first segment content/i
+  const tab2Content = /second segment content/i
+  const tab3Content = /third segment content/i
+  await expect(tab1).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab2).toHaveAttribute("aria-selected", "true")
+  await expect(tab3).not.toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).not.toBeInTheDocument()
+  await userEvent.click(tab1)
+  await expect(tab1).toHaveAttribute("aria-selected", "true")
+  await expect(tab2).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab3).not.toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).not.toBeInTheDocument()
+  await userEvent.click(tab3)
+  await expect(tab1).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab2).not.toHaveAttribute("aria-selected", "true")
+  await expect(tab3).toHaveAttribute("aria-selected", "true")
+  await expect(canvas.queryByText(tab1Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab2Content)).not.toBeInTheDocument()
+  await expect(canvas.queryByText(tab3Content)).toBeInTheDocument()
 }
--- src/components/controls/segments/Segments/Segments.tsx
+++ src/components/controls/segments/Segments/Segments.tsx
@@ -1,70 +1,99 @@
 import styled from "@emotion/styled"
 import {
+  ComponentPropsWithoutRef,
   ElementType,
   forwardRef,
-  HTMLAttributes,
   KeyboardEvent,
   ReactNode,
   useRef,
-  useState,
 } from "react"
 import { Segment } from "../Segment/Segment"
+import { useSegmentState } from "./useSegmentsState"
 
-interface SegmentsProps extends HTMLAttributes<HTMLDivElement> {
+interface SegmentsProps extends ComponentPropsWithoutRef<"div"> {
+  /** Effective element used. */
   as?: ElementType
-  segments: {
-    content: ReactNode
-    id: string
-    isDisabled?: boolean
-    text: string
-  }[]
+
+  /** The value of the segment that should be active when initially rendered. Use when you do not need to control the state of the tabs. */
+  defaultValue?: number
+
+  /** Event handler called when the open segment index changes. */
+  onValueChange?: (value: number) => void
+
+  /** What segments do show. */
+  segments: Array<Segment>
+
+  /** Controlled open segment index. */
+  value?: number
 }
 
-export const Segments = forwardRef<HTMLDivElement, SegmentsProps>(({ segments, ...props }, ref) => {
-  const [selectedSegmentIndex, setSelectedSegmentIndex] = useState(0)
-  const segmentRefs: HTMLButtonElement[] = []
-  const contentRef = useRef<HTMLElement>(null)
+export const Segments = forwardRef<HTMLDivElement, SegmentsProps>(
+  ({ defaultValue, onValueChange, segments, value: valueProp, ...props }, ref) => {
+    const { value, handleChange } = useSegmentState({
+      value: valueProp,
+      defaultValue,
+      onChange: onValueChange,
+    })
+    const segmentRefs: HTMLButtonElement[] = []
+    const contentRef = useRef<HTMLElement>(null)
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLButtonElement>): void => {
-    if (e.code === "ArrowLeft" && selectedSegmentIndex > 0) {
-      const newIndex = selectedSegmentIndex - 1
-      setSelectedSegmentIndex(newIndex)
-      segmentRefs[newIndex].focus()
-    } else if (e.code === "ArrowRight" && selectedSegmentIndex < segments.length - 1) {
-      const newIndex = selectedSegmentIndex + 1
-      setSelectedSegmentIndex(newIndex)
-      segmentRefs[newIndex].focus()
-    } else if (e.code === "ArrowDown") {
-      contentRef.current?.focus()
+    const handleKeyDown = (e: KeyboardEvent<HTMLButtonElement>): void => {
+      if (e.code === "ArrowLeft" && value > 0) {
+        const newIndex = value - 1
+        handleChange(newIndex)
+        segmentRefs[newIndex].focus()
+      } else if (e.code === "ArrowRight" && value < segments.length - 1) {
+        const newIndex = value + 1
+        handleChange(newIndex)
+        segmentRefs[newIndex].focus()
+      } else if (e.code === "ArrowDown") {
+        contentRef.current?.focus()
+      }
     }
-  }
 
-  const selectedSegmentContent = segments[selectedSegmentIndex].content
+    // const getSelectedSegmentContent = (): ReactNode => {
+    //   if (openIndex && segments[openIndex]) return segments[openIndex].content
+    //   return segments[selectedSegmentIndex].
+    // }
 
-  return (
-    <>
-      <SegmentsWrapper role="tablist" {...props} ref={ref}>
-        {segments.map((segment, index) => (
-          <Segment
-            type="button"
-            onClick={() => setSelectedSegmentIndex(index)}
-            onKeyDown={handleKeyDown}
-            key={segment.id}
-            tabIndex={index === selectedSegmentIndex ? undefined : -1}
-            aria-selected={index === selectedSegmentIndex ? "true" : undefined}
-            disabled={segment.isDisabled}
-            ref={(element) => element && segmentRefs.push(element)}
-          >
-            {segment.text}
-          </Segment>
-        ))}
-      </SegmentsWrapper>
-      <Content ref={contentRef} tabIndex={-1}>
-        {selectedSegmentContent}
-      </Content>
-    </>
-  )
-})
+    const handleClick = (index: number): void => {
+      handleChange(index)
+    }
+
+    const selectedSegmentContent = segments[value].content
+
+    return (
+      <>
+        <SegmentsWrapper role="tablist" {...props} ref={ref}>
+          {segments.map((segment, index) => (
+            <Segment
+              type="button"
+              onClick={() => handleClick(index)}
+              onKeyDown={handleKeyDown}
+              key={segment.id}
+              tabIndex={index === value ? undefined : -1}
+              aria-selected={index === value ? "true" : undefined}
+              disabled={segment.isDisabled}
+              ref={(element) => element && segmentRefs.push(element)}
+            >
+              {segment.text}
+            </Segment>
+          ))}
+        </SegmentsWrapper>
+        <Content ref={contentRef} tabIndex={-1}>
+          {selectedSegmentContent}
+        </Content>
+      </>
+    )
+  },
+)
+
+interface Segment {
+  content: ReactNode
+  id: string
+  isDisabled?: boolean
+  text: string
+}
 
 const SegmentsWrapper = styled.div`
   display: grid;
