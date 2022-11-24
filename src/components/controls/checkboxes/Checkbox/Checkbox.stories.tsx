0a6a9cfa07d69793d7b291e483fc442521ccca27
Filip Tammergård <filip@tammergard.se>
2022-11-24 12:18:47 +0100

    chore(Checkbox): add interaction tests

--- src/components/controls/checkboxes/Checkbox/Checkbox.stories.tsx
+++ src/components/controls/checkboxes/Checkbox/Checkbox.stories.tsx
@@ -16,54 +16,158 @@ export default {
 
 const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />
 
-export const Basic = Template.bind({})
-Basic.args = {
+export const WithLabel = Template.bind({})
+WithLabel.args = {
   children: "Label",
 }
-Basic.play = async ({ canvasElement }) => {
+WithLabel.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const checkbox = canvas.getByRole("checkbox", { name: "Label" })
-  await expect(checkbox).toBeInTheDocument()
+  await expect(checkbox).not.toBeChecked()
+}
+
+export const DefaultChecked = Template.bind({})
+DefaultChecked.args = {
+  ...WithLabel.args,
+  checked: true,
+}
+DefaultChecked.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const checkbox = canvas.getByRole("checkbox", { name: "Label" })
+  await expect(checkbox).toBeChecked()
 }
 
 const ControlledTemplate: ComponentStory<typeof Checkbox> = (args) => {
-  const [checked, setChecked] = useState(true)
+  const [checked, setChecked] = useState(false)
   return <Checkbox {...args} checked={checked} onCheckedChange={setChecked} />
 }
 
 export const Controlled = ControlledTemplate.bind({})
 Controlled.args = {
-  ...Basic.args,
+  ...WithLabel.args,
 }
 Controlled.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const checkbox = canvas.getByRole("checkbox", { name: "Label" })
-  await expect(checkbox).toBeInTheDocument()
+  await expect(checkbox).not.toBeChecked()
 }
 
 export const Mouse = Template.bind({})
 Mouse.args = {
-  ...Basic.args,
+  ...WithLabel.args,
 }
 Mouse.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const checkbox = canvas.getByRole("checkbox", { name: "Label" })
   await expect(checkbox).not.toBeChecked()
+  await expect(checkbox).not.toHaveFocus()
   await userEvent.click(checkbox)
   await expect(checkbox).toBeChecked()
+  await expect(checkbox).toHaveFocus()
   await userEvent.click(checkbox)
   await expect(checkbox).not.toBeChecked()
 }
 
 export const Keyboard = ControlledTemplate.bind({})
 Keyboard.args = {
-  ...Basic.args,
+  ...WithLabel.args,
 }
 Keyboard.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const checkbox = canvas.getByRole("checkbox", { name: "Label" })
-  await expect(checkbox).toBeChecked()
+  await expect(checkbox).not.toBeChecked()
   await expect(checkbox).not.toHaveFocus()
   await userEvent.tab()
   await expect(checkbox).toHaveFocus()
+  await expect(checkbox).not.toBeChecked()
+  await userEvent.keyboard("[Space]")
+  await expect(checkbox).toBeChecked()
+}
+
+const GroupTemplate: ComponentStory<typeof Checkbox> = () => {
+  return (
+    <>
+      <Checkbox name="name">Label 1</Checkbox>
+      <Checkbox name="name">Label 2</Checkbox>
+      <Checkbox name="name">Label 3</Checkbox>
+    </>
+  )
+}
+export const GroupMouse = GroupTemplate.bind({})
+GroupMouse.args = {
+  ...WithLabel.args,
+}
+GroupMouse.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const checkbox1 = canvas.getByRole("checkbox", { name: "Label 1" })
+  const checkbox2 = canvas.getByRole("checkbox", { name: "Label 2" })
+  const checkbox3 = canvas.getByRole("checkbox", { name: "Label 3" })
+  await expect(checkbox1).not.toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await userEvent.click(checkbox1)
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await userEvent.click(checkbox2)
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await userEvent.click(checkbox3)
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).toBeChecked()
+  await expect(checkbox3).toBeChecked()
+  await userEvent.click(checkbox2)
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).toBeChecked()
+}
+
+export const GroupKeyboard = GroupTemplate.bind({})
+GroupKeyboard.args = {
+  ...WithLabel.args,
+}
+GroupKeyboard.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const checkbox1 = canvas.getByRole("checkbox", { name: "Label 1" })
+  const checkbox2 = canvas.getByRole("checkbox", { name: "Label 2" })
+  const checkbox3 = canvas.getByRole("checkbox", { name: "Label 3" })
+  await expect(checkbox1).not.toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await expect(checkbox1).not.toHaveFocus()
+  await expect(checkbox2).not.toHaveFocus()
+  await expect(checkbox3).not.toHaveFocus()
+  await userEvent.tab()
+  await expect(checkbox1).toHaveFocus()
+  await expect(checkbox2).not.toHaveFocus()
+  await expect(checkbox3).not.toHaveFocus()
+  await userEvent.keyboard("[Space]")
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await userEvent.tab()
+  await expect(checkbox1).not.toHaveFocus()
+  await expect(checkbox2).toHaveFocus()
+  await expect(checkbox3).not.toHaveFocus()
+  await userEvent.keyboard("[Space]")
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).toBeChecked()
+  await expect(checkbox3).not.toBeChecked()
+  await userEvent.tab()
+  await expect(checkbox1).not.toHaveFocus()
+  await expect(checkbox2).not.toHaveFocus()
+  await expect(checkbox3).toHaveFocus()
+  await userEvent.keyboard("[Space]")
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).toBeChecked()
+  await expect(checkbox3).toBeChecked()
+  await userEvent.tab({ shift: true })
+  await expect(checkbox1).not.toHaveFocus()
+  await expect(checkbox2).toHaveFocus()
+  await expect(checkbox3).not.toHaveFocus()
+  await userEvent.keyboard("[Space]")
+  await expect(checkbox1).toBeChecked()
+  await expect(checkbox2).not.toBeChecked()
+  await expect(checkbox3).toBeChecked()
 }
