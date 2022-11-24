5a37e926fff8175657a0c50b6c4dc48a14e3bbb8
Filip Tammergård <filip@tammergard.se>
2022-11-24 14:18:10 +0100

    chore(Link): add interaction test

--- src/components/typography/Link/Link.stories.tsx
+++ src/components/typography/Link/Link.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { Link } from "./Link"
 
 export default {
@@ -10,5 +12,12 @@ const Template: ComponentStory<typeof Link> = (args) => <Link {...args} />
 
 export const Basic = Template.bind({})
 Basic.args = {
+  as: "a",
   children: "A whole new way to ship.",
+  href: "https://einride.tech",
+}
+Basic.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const link = canvas.getByRole("link", { name: "A whole new way to ship." })
+  await expect(link).not.toBeDisabled()
 }
