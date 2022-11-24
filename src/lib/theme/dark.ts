1dc5ab97c36bfeb70c7cb6557f75174b800cec3d
Filip Tammergård <filip@tammergard.se>
2022-11-24 12:27:50 +0100

    fix(theme): update primaryInverted content color

--- src/lib/theme/dark.ts
+++ src/lib/theme/dark.ts
@@ -25,7 +25,7 @@ export const dark: ColorTheme = {
   },
   content: {
     primary: color.greyscale.white,
-    primaryInverted: color.greyscale.grey100,
+    primaryInverted: color.greyscale.black,
     secondary: `${color.greyscale.grey20}B3`, // B3 is HEX for ~70% opacity
     tertiary: `${color.greyscale.grey20}66`, // 66 is HEX for ~40% opacity
     positive: color.green.light,
