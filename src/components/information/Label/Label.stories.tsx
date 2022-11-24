956774b5a94558ac2e6d6c4fc86b3a1d3a58bcd6
Filip Tammergård <filip@tammergard.se>
2022-11-24 15:41:52 +0100

    chore(Label): add interaction tests

--- src/components/information/Label/Label.stories.tsx
+++ src/components/information/Label/Label.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { Label } from "./Label"
 
 export default {
@@ -8,19 +10,34 @@ export default {
 
 const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Basic = Template.bind({})
+Basic.args = {
   children: "Label",
+}
+Basic.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const label = canvas.getByText("Label")
+  await expect(label).toBeInTheDocument()
 }
 
 export const Positive = Template.bind({})
 Positive.args = {
-  children: "Label",
+  ...Basic.args,
   variant: "positive",
+}
+Positive.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const label = canvas.getByText("Label")
+  await expect(label).toBeInTheDocument()
 }
 
 export const Negative = Template.bind({})
 Negative.args = {
-  children: "Label",
+  ...Basic.args,
   variant: "negative",
+}
+Negative.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const label = canvas.getByText("Label")
+  await expect(label).toBeInTheDocument()
 }
