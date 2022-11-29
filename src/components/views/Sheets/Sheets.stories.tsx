850866a55d3e7ff12120f68981db5248394367ae
Filip Tammergård <filip@tammergard.se>
2022-11-29 12:38:51 +0100

    feat(Sheets): base on radix-ui

--- src/components/views/Sheets/Sheets.stories.tsx
+++ src/components/views/Sheets/Sheets.stories.tsx
@@ -1,7 +1,9 @@
 import { useDisclosure } from "@einride/hooks"
+import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
 import { PrimaryButton } from "../../controls/buttons/PrimaryButton/PrimaryButton"
-import { Paragraph } from "../../typography/Paragraph/Paragraph"
+import { Text } from "../../typography/Text/Text"
 import { Sheets } from "./Sheets"
 
 export default {
@@ -11,10 +13,9 @@ export default {
 
 const Template: ComponentStory<typeof Sheets> = (args) => {
   const { isOpen, handlers } = useDisclosure(true)
-
   return (
     <>
-      <PrimaryButton onClick={handlers.open}>Open</PrimaryButton>
+      <PrimaryButton onClick={handlers.open}>Open sheets</PrimaryButton>
       <Sheets
         {...args}
         closeHandler={handlers.close}
@@ -25,23 +26,70 @@ const Template: ComponentStory<typeof Sheets> = (args) => {
           onClick: handlers.close,
         }}
         navigationTitle="Page name"
-        primaryAction={{ children: "Primary" }}
-        secondaryAction={{ children: "Close", onClick: handlers.close }}
       >
-        <Paragraph>Sheets content</Paragraph>
+        <Text>Sheets content</Text>
       </Sheets>
     </>
   )
 }
 
-const parameters = {
-  controls: {
-    exclude: /on*/,
-  },
+export const Basic = Template.bind({})
+Basic.args = {}
+Basic.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement.parentElement ?? canvasElement)
+  const alert = canvas.getByRole("dialog")
+  await expect(alert).toBeInTheDocument()
 }
 
-export const Default = Template.bind({})
-Default.args = {
-  size: "md",
+const NavigationTemplate: ComponentStory<typeof Sheets> = (args) => {
+  const { isOpen, handlers } = useDisclosure(false)
+  return (
+    <>
+      <PrimaryButton onClick={handlers.open}>Open sheets</PrimaryButton>
+      <Sheets
+        {...args}
+        closeHandler={handlers.close}
+        isOpen={isOpen}
+        navigationAction={{
+          "aria-label": "Close",
+          icon: "xMark",
+          onClick: handlers.close,
+        }}
+        navigationTitle="Page name"
+      >
+        <Text>Sheets content</Text>
+      </Sheets>
+    </>
+  )
 }
-Default.parameters = parameters
+
+export const Mouse = NavigationTemplate.bind({})
+Mouse.args = {}
+Mouse.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement.parentElement ?? canvasElement)
+  const openButton = canvas.getByRole("button", { name: "Open sheets" })
+  await userEvent.click(openButton)
+  const dialog = canvas.getByRole("dialog")
+  await expect(dialog).toBeInTheDocument()
+  await expect(openButton).toHaveStyle("pointer-events: none")
+  const closeButton = canvas.getByRole("button", { name: "Close" })
+  await userEvent.click(closeButton)
+  await expect(dialog).not.toBeInTheDocument()
+}
+
+export const Keyboard = NavigationTemplate.bind({})
+Keyboard.args = {}
+Keyboard.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement.parentElement ?? canvasElement)
+  const openButton = canvas.getByRole("button", { name: "Open sheets" })
+  await expect(openButton).not.toHaveFocus()
+  await userEvent.tab()
+  await expect(openButton).toHaveFocus()
+  await userEvent.keyboard("[Enter]")
+  const dialog = canvas.getByRole("dialog")
+  await expect(dialog).toBeInTheDocument()
+  const closeButton = canvas.getByRole("button", { name: "Close" })
+  await expect(closeButton).toHaveFocus()
+  await userEvent.keyboard("[Enter]")
+  await expect(dialog).not.toBeInTheDocument()
+}
