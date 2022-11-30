6d51e50f40c426c6a5636fc8ecdb0e208a0e6fd7
Filip Tammergård <filip@tammergard.se>
2022-11-30 10:20:51 +0100

    feat(loaders): make responsive

--- src/components/feedback/Loader/Loader.tsx
+++ src/components/feedback/Loader/Loader.tsx
@@ -1,25 +1,27 @@
 import styled from "@emotion/styled"
 import { motion } from "framer-motion"
-import { HTMLAttributes } from "react"
+import { ComponentPropsWithoutRef, forwardRef } from "react"
 
-export type LoaderProps = HTMLAttributes<HTMLDivElement>
+export type LoaderProps = ComponentPropsWithoutRef<"div">
 
-export const Loader = (props: LoaderProps): JSX.Element => {
-  return (
-    <Wrapper {...props}>
-      <Line
-        animate={{
-          rotate: 180,
-          transition: {
-            repeat: Infinity,
-            ease: [0.84, 0.0, 0.33, 1.0],
-            duration: 1,
-          },
-        }}
-      />
-    </Wrapper>
-  )
-}
+export const Loader = forwardRef<HTMLDivElement, LoaderProps>(
+  (props, forwardedRef): JSX.Element => {
+    return (
+      <Wrapper {...props} ref={forwardedRef}>
+        <Line
+          animate={{
+            rotate: 180,
+            transition: {
+              repeat: Infinity,
+              ease: [0.84, 0.0, 0.33, 1.0],
+              duration: 1,
+            },
+          }}
+        />
+      </Wrapper>
+    )
+  },
+)
 
 const Wrapper = styled.div`
   position: relative;
