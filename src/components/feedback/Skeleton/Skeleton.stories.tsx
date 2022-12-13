6495e75d0a0c5396dbb24abb6617299c44424b07
Filip Tammergård <filip@tammergard.se>
2022-12-13 13:09:17 +0100

    feat(Skeleton): make it possible to infer dimensions

--- src/components/feedback/Skeleton/Skeleton.stories.tsx
+++ src/components/feedback/Skeleton/Skeleton.stories.tsx
@@ -8,5 +8,23 @@ export default {
 
 const Template: ComponentStory<typeof Skeleton> = (args) => <Skeleton {...args} />
 
-export const Default = Template.bind({})
-Default.args = {}
+export const Rectangle = Template.bind({})
+Rectangle.args = {}
+
+export const Circle = Template.bind({})
+Circle.args = {
+  shape: "circle",
+  height: "lg",
+}
+
+export const Content = Template.bind({})
+Content.args = {
+  children:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolor nihil amet tempore magnam optio, numquam nostrum inventore tempora assumenda saepe, aut repellat. Temporibus aspernatur aperiam magnam debitis facere odio?",
+  height: "auto",
+}
+
+export const WithoutAnimation = Template.bind({})
+WithoutAnimation.args = {
+  animate: false,
+}
--- src/components/feedback/Skeleton/Skeleton.tsx
+++ src/components/feedback/Skeleton/Skeleton.tsx
@@ -1,23 +1,35 @@
+import { css, keyframes } from "@emotion/react"
 import styled from "@emotion/styled"
 import { forwardRef } from "react"
 import { SpacingInput } from "../../../lib/theme/props"
 import { Box, BoxProps } from "../../layout/Box/Box"
 
 interface SkeletonProps extends BoxProps {
+  /** Whether or not to animate the skeleton. */
+  animate?: boolean
+
   /** Height of the skeleton. Default is `md`. */
-  height?: SpacingInput
+  height?: SpacingInput | "auto"
 
   /** Shape of the skeleton. Default is `rectangle`. */
   shape?: Shape
+
+  /** Determines whether skeleton overlay should be displayed. */
+  visible?: boolean
 }
 
 export const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
-  ({ height = "md", shape = "rectangle", ...props }, forwardedRef): JSX.Element => {
+  (
+    { animate = true, height = "md", shape = "rectangle", visible = true, ...props },
+    forwardedRef,
+  ): JSX.Element => {
     return (
       <Wrapper
+        animate={animate}
         height={height}
         inlineSize={shape === "circle" ? height : "100%"}
         shape={shape}
+        visible={visible}
         {...props}
         ref={forwardedRef}
       />
@@ -27,12 +39,50 @@ export const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
 
 type Shape = "rectangle" | "circle"
 
+const pulse = keyframes`
+  from, to {
+    opacity: 0.1;
+  }
+
+  50% {
+    opacity: 0.3;
+  }
+`
+
 interface WrapperProps {
+  animate?: boolean
   shape: Shape
+  visible?: boolean
 }
 
 const Wrapper = styled(Box)<WrapperProps>`
-  background: ${({ theme }) => theme.colors.background.secondary};
-  border-radius: ${({ shape, theme }) =>
-    shape === "circle" ? theme.borderRadii.full : theme.borderRadii.sm};
+  position: relative;
+
+  ${({ shape, visible, theme }) =>
+    visible &&
+    css`
+      &::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background: ${theme.colors.background.secondary};
+        border-radius: ${shape === "circle" ? theme.borderRadii.full : theme.borderRadii.sm};
+      }
+    `}
+
+  @media (prefers-reduced-motion: no-preference) {
+    ${({ animate, shape, visible, theme }) =>
+      visible &&
+      animate &&
+      css`
+        &::after {
+          content: "";
+          position: absolute;
+          inset: 0;
+          background: ${theme.colors.content.tertiary};
+          border-radius: ${shape === "circle" ? theme.borderRadii.full : theme.borderRadii.sm};
+          animation: ${pulse} 1500ms linear infinite;
+        }
+      `}
+  }
 `
