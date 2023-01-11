4f293727116ed23a1a6101a7159748314c29f295
Alexander Scholz <alexander.scholz@einride.tech>
2023-01-11 09:38:35 +0100

    feat(Calendar): add standalone calendar component

---
+++ src/components/controls/dates/Calendar/Calendar.tsx
@@ -0,0 +1,154 @@
+import styled from "@emotion/styled"
+import { Calendar as MantineCalendar } from "@mantine/dates"
+import { useTheme } from "../../../../hooks/useTheme"
+import { Box, BoxProps } from "../../../layout/Box/Box"
+
+interface CalendarBaseProps {
+  /** Maximum possible date. */
+  maxDate?: Date
+
+  /** Called when date changes. */
+  onChange: (value: CalendarValue) => void
+
+  /** Selected date, required with controlled input. */
+  value: CalendarValue
+
+  /** Props passed to root element. */
+  wrapperProps?: BoxProps
+}
+
+export type CalendarProps = CalendarBaseProps
+
+export const Calendar = ({ wrapperProps, ...props }: CalendarProps): JSX.Element => {
+  const theme = useTheme()
+  return (
+    <Box {...wrapperProps}>
+      <StyledCalendar
+        allowLevelChange={false}
+        dayStyle={() => ({
+          fontFamily: theme.fonts.body,
+          fontSize: theme.fontSizes.md,
+          fontWeight: theme.fontWeights.book,
+        })}
+        dayClassName={(date) => (date.toDateString() === new Date().toDateString() ? "today" : "")}
+        {...props}
+      />
+    </Box>
+  )
+}
+
+type CalendarValue = Date | null
+
+const StyledCalendar = styled(MantineCalendar)`
+  &.mantine-Calendar-calendarBase {
+    background: ${({ theme }) => theme.colors.background.secondaryElevated};
+    border: none;
+    border-radius: ${({ theme }) => theme.borderRadii.lg};
+    padding: ${({ theme }) => 2 * theme.spacingBase}rem;
+    box-shadow: none;
+    max-width: none;
+    display: inline-flex;
+
+    .mantine-Calendar-calendarHeader {
+      display: grid;
+      align-items: center;
+      gap: ${({ theme }) => theme.spacingBase}rem;
+      grid-template-areas: "text previous-month next-month";
+      grid-template-columns: ${({ theme }) => 18.75 * theme.spacingBase}rem auto auto;
+      white-space: nowrap;
+
+      .mantine-Calendar-calendarHeaderLevel {
+        grid-area: text;
+        font-size: ${({ theme }) => theme.fontSizes.md};
+        color: ${({ theme }) => theme.colors.content.primary};
+        padding: 0;
+        justify-content: start;
+      }
+    }
+
+    .mantine-Calendar-calendarHeaderControl {
+      background: ${({ theme }) => theme.colors.buttons.background.tertiary};
+      color: ${({ theme }) => theme.colors.content.primary};
+      border-radius: ${({ theme }) => theme.borderRadii.full};
+      block-size: ${({ theme }) => 6 * theme.spacingBase}rem;
+      inline-size: ${({ theme }) => 6 * theme.spacingBase}rem;
+      transform: none;
+      &:hover {
+        background: ${({ theme }) => theme.colors.buttons.background.hover.tertiary};
+      }
+      &:active {
+        background: ${({ theme }) => theme.colors.buttons.background.active.tertiary};
+      }
+      &:focus-visible {
+        outline: none;
+        background: ${({ theme }) => theme.colors.buttons.background.focused.tertiary};
+        border: ${({ theme }) => theme.spacingBase / 8}rem solid
+          ${({ theme }) => theme.colors.border.selected};
+      }
+      svg {
+        display: none;
+      }
+      &:first-of-type {
+        grid-area: previous-month;
+        &::before {
+          font-family: ${({ theme }) => theme.fonts.body};
+          font-size: ${({ theme }) => theme.fontSizes.md};
+          content: "←";
+        }
+      }
+      &:last-of-type {
+        gria-area: next-month;
+        &::before {
+          font-family: ${({ theme }) => theme.fonts.body};
+          font-size: ${({ theme }) => theme.fontSizes.md};
+          content: "→";
+        }
+      }
+    }
+    .mantine-Calendar-weekday {
+      color: ${({ theme }) => theme.colors.content.secondary};
+      font-size: ${({ theme }) => theme.fontSizes.md};
+      font-weight: ${({ theme }) => theme.fontWeights.book};
+    }
+    .mantine-Calendar-day {
+      border-radius: ${({ theme }) => theme.borderRadii.sm};
+      color: ${({ theme }) => theme.colors.content.primary};
+      margin-block-start: ${({ theme }) => theme.spacingBase}rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &.today {
+        color: ${({ theme }) => theme.colors.content.positive};
+      }
+      &[data-outside] {
+        display: none;
+      }
+      &:hover {
+        background: ${({ theme }) => theme.colors.background.tertiary};
+        color: ${({ theme }) => theme.colors.content.primary};
+      }
+      &[data-selected] {
+        background: ${({ theme }) => theme.colors.background.primaryInverted};
+        color: ${({ theme }) => theme.colors.content.primaryInverted};
+
+        &:focus-visible {
+          background: ${({ theme }) => theme.colors.background.primaryInverted};
+          color: ${({ theme }) => theme.colors.content.primaryInverted};
+          text-decoration: underline;
+        }
+      }
+      &:disabled {
+        color: ${({ theme }) => theme.colors.content.tertiary};
+      }
+      &:focus-visible {
+        outline: none;
+        background: ${({ theme }) => theme.colors.background.tertiary};
+        color: ${({ theme }) => theme.colors.content.primary};
+        border: ${({ theme }) => theme.spacingBase / 8}rem solid
+          ${({ theme }) => theme.colors.border.selected};
+        text-decoration: underline;
+      }
+    }
+  }
+`
