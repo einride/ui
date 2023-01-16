c2d09d9e728371af7b03edd2c4161f1fbf0896bd
Filip Tammergård <filip@tammergard.se>
2023-01-16 12:52:48 +0100

    fix: configure vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 /// <reference types="vitest" />
+import { resolve } from "path"
+import typescript from "@rollup/plugin-typescript"
 import { defineConfig } from "vite"
+import pkg from "./package.json"
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies ?? {}),
+        ...Object.keys(pkg.peerDependencies ?? {}),
+        "react/jsx-runtime",
+      ],
+      plugins: [typescript({ tsconfig: resolve(__dirname, "./tsconfig.build.json") })],
+    },
+  },
   test: {
     environment: "happy-dom",
   },
