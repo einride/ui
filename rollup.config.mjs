273b041cec3d134407f195384dbaf102ede1e138
Filip Tammergård <filip@tammergard.se>
2023-01-23 13:18:56 +0100

    fix: use custom rollup config until vite esm issue is resolved

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import image from "@rollup/plugin-image"
+import typescript from "@rollup/plugin-typescript"
+import pkg from "./package.json" assert { type: "json" }
+
+export default {
+  input: "src/main.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies ?? {}),
+    ...Object.keys(pkg.peerDependencies ?? {}),
+    "react/jsx-runtime",
+  ],
+  plugins: [image(), typescript({ tsconfig: "./tsconfig.build.json" })],
+}
