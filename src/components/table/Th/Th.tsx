08b14161a82cb381d830239fe31b20524261c03a
Filip Tammergård <filip@tammergard.se>
2023-01-27 12:49:28 +0100

    feat(Th): make it possible to set text-align

--- src/components/table/Th/Th.tsx
+++ src/components/table/Th/Th.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled"
 import { ComponentPropsWithoutRef, forwardRef } from "react"
 import { getColor, getFont } from "../../../lib/theme/prop-system"
-import { Color, FontFamily } from "../../../lib/theme/props"
+import { Color, FontFamily, TextAlign } from "../../../lib/theme/props"
 
 interface ThProps extends Omit<ComponentPropsWithoutRef<"th">, "color"> {
   /** Text color set on the table header. */
@@ -12,12 +12,15 @@ interface ThProps extends Omit<ComponentPropsWithoutRef<"th">, "color"> {
 
   /** Indicates what cells the table header element relates to. */
   scope: "col" | "row"
+
+  /** `text-align` CSS property. Set to `end` to end-align content in table header. Default is `start`.  */
+  textAlign?: TextAlign
 }
 
 export const Th = forwardRef<HTMLTableCellElement, ThProps>(
-  ({ children, color, ...props }, ref) => {
+  ({ children, color, textAlign = "start", ...props }, ref) => {
     return (
-      <StyledTh textColor={color} {...props} ref={ref}>
+      <StyledTh textAlign={textAlign} textColor={color} {...props} ref={ref}>
         {children}
       </StyledTh>
     )
@@ -26,6 +29,7 @@ export const Th = forwardRef<HTMLTableCellElement, ThProps>(
 
 interface StyledThProps {
   font?: FontFamily
+  textAlign: TextAlign | undefined
   textColor: Color | undefined
 }
 
@@ -36,5 +40,5 @@ const StyledTh = styled.th<StyledThProps>`
   line-height: calc(4 / 3);
   padding-inline: ${({ theme }) => 2 * theme.spacingBase}rem;
   block-size: ${({ theme }) => 10 * theme.spacingBase}rem;
-  text-align: start;
+  text-align: ${({ textAlign }) => textAlign};
 `
