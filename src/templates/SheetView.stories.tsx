e81dbeca46345a9f44d742903b27404465807f32
Filip Tammergård <filip@tammergard.se>
2023-01-31 11:31:02 +0100

    chore: add sheet view template

---
+++ src/templates/SheetView.stories.tsx
@@ -0,0 +1,55 @@
+import { useDisclosure } from "@einride/hooks"
+import { PrimaryButton } from "../components/controls/buttons/PrimaryButton/PrimaryButton"
+import { TextInput } from "../components/controls/inputs/TextInput/TextInput"
+import { Select } from "../components/controls/selects/Select/Select"
+import { Box } from "../components/layout/Box/Box"
+import { Stack } from "../components/layout/Stack/Stack"
+import { VerticalSpacing } from "../components/layout/VerticalSpacing/VerticalSpacing"
+import { Text } from "../components/typography/Text/Text"
+import { Sheets } from "../components/views/Sheets/Sheets"
+import { Navbar } from "./components/Navbar.stories"
+import { UserTable } from "./components/UserTable.stories"
+
+export default {
+  title: "Templates",
+  args: { style: { paddingBlockStart: 0 } },
+}
+
+export const SheetView = (): JSX.Element => {
+  const { isOpen, handlers } = useDisclosure(true)
+
+  return (
+    <>
+      <Box>
+        <Navbar actions={<PrimaryButton onClick={handlers.open}>Invite</PrimaryButton>} />
+        <VerticalSpacing />
+        <Text as="h1" variant="titleLg">
+          Title
+        </Text>
+        <Text as="h2" color="secondary" variant="titleLg">
+          Subtitle
+        </Text>
+        <VerticalSpacing size="lg" />
+        <UserTable />
+      </Box>
+      <Sheets
+        isOpen={isOpen}
+        closeHandler={handlers.close}
+        primaryAction={{ children: "Invite user" }}
+        secondaryAction={{ children: "Cancel", onClick: handlers.close }}
+      >
+        <VerticalSpacing size="sm" />
+        <Stack gap="sm">
+          <TextInput label="Name" />
+          <TextInput label="Email" />
+          <Select label="Role">
+            <option>Owner</option>
+            <option>Admin</option>
+            <option>Editor</option>
+            <option>Viewer</option>
+          </Select>
+        </Stack>
+      </Sheets>
+    </>
+  )
+}
--- src/templates/TableView.stories.tsx
+++ src/templates/TableView.stories.tsx
@@ -1,41 +1,13 @@
-import { faker } from "@faker-js/faker"
-import { Icon } from "../components/content/Icon/Icon"
-import { IconButton } from "../components/controls/buttons/IconButton/IconButton"
 import { Box } from "../components/layout/Box/Box"
 import { VerticalSpacing } from "../components/layout/VerticalSpacing/VerticalSpacing"
-import * as Menu from "../components/menus/Menu"
-import { Table } from "../components/table/Table/Table"
-import { Tbody } from "../components/table/Tbody/Tbody"
-import { Td } from "../components/table/Td/Td"
-import { Th } from "../components/table/Th/Th"
-import { Thead } from "../components/table/Thead/Thead"
-import { Tr } from "../components/table/Tr/Tr"
 import { Text } from "../components/typography/Text/Text"
-import { Navbar } from "./components/Navbar/Navbar"
+import { Navbar } from "./components/Navbar.stories"
+import { UserTable } from "./components/UserTable.stories"
 
 export default {
   title: "Templates",
   args: { style: { paddingBlockStart: 0 } },
 }
-
-function createRandomUser(): {
-  id: string
-  name: string
-  email: string
-  number: number
-  role: string
-} {
-  return {
-    id: faker.datatype.uuid(),
-    name: faker.name.fullName(),
-    email: faker.internet.email(),
-    number: faker.datatype.float(),
-    role: faker.helpers.arrayElement(["Owner", "Admin", "Editor", "Viewer"]),
-  }
-}
-
-const users = Array.from({ length: 20 }).map(() => createRandomUser())
-
 export const TableView = (): JSX.Element => (
   <Box>
     <Navbar />
@@ -47,38 +19,6 @@ export const TableView = (): JSX.Element => (
       Subtitle
     </Text>
     <VerticalSpacing size="lg" />
-    <Table>
-      <Thead>
-        <Tr color="secondary">
-          <Th scope="row">User</Th>
-          <Th scope="row">Email</Th>
-          <Th scope="row">Role</Th>
-          <Th scope="row" textAlign="end">
-            Number
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {users.map((user) => (
-          <Tr key={user.id}>
-            <Td>{user.name}</Td>
-            <Td>{user.email}</Td>
-            <Td>{user.role}</Td>
-            <Td textAlign="end">{user.number}</Td>
-            <Td textAlign="end">
-              <Menu.Root>
-                <Menu.Trigger>
-                  <IconButton aria-label={`${user.name} options`} icon="ellipsis" />
-                </Menu.Trigger>
-                <Menu.Content>
-                  <Menu.Item icon={<Icon name="arrowRight" />}>Label</Menu.Item>
-                  <Menu.Item icon={<Icon name="arrowRight" />}>Label</Menu.Item>
-                </Menu.Content>
-              </Menu.Root>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <UserTable />
   </Box>
 )
