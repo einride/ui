7c0691ecfebb0ad3fd10f2a95cc2f75b7562a112
Filip Tammergård <filip@tammergard.se>
2023-02-06 19:29:58 +0100

    chore: add snapshot stories for all buttons

--- src/components/controls/buttons/LinkButton/LinkButton.stories.tsx
+++ src/components/controls/buttons/LinkButton/LinkButton.stories.tsx
@@ -1,6 +1,7 @@
 import { expect } from "@storybook/jest"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
 import { within } from "@storybook/testing-library"
+import { SnapshotWrapper } from "../../../../lib/storybook/SnapshotWrapper"
 import { LinkButton } from "./LinkButton"
 
 export default {
@@ -18,4 +19,18 @@ Basic.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const button = canvas.getByRole("button", { name: "Button" })
   await expect(button).not.toBeDisabled()
+}
+
+export const Snapshot = (): JSX.Element => (
+  <SnapshotWrapper>
+    {[Basic].map((Story, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <Story key={index} {...Story.args}>
+        {Story.args?.children}
+      </Story>
+    ))}
+  </SnapshotWrapper>
+)
+Snapshot.parameters = {
+  chromatic: { disableSnapshot: false },
 }
