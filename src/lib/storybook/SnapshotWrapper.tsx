52b932327947155045a931f6224092a72e334fe6
Alexander Scholz <alexander.scholz@einride.tech>
2023-02-08 14:52:40 +0100

    chore: render snapshots in light and dark mode

--- src/lib/storybook/SnapshotWrapper.tsx
+++ src/lib/storybook/SnapshotWrapper.tsx
@@ -1,10 +1,35 @@
+import styled from "@emotion/styled"
 import { ReactNode } from "react"
 import { Stack } from "../../components/layout/Stack/Stack"
+import { EinrideProvider } from "../../contexts/EinrideProvider"
+import { einrideTheme } from "../theme/einride"
 
 interface SnapshotWrapperProps {
   children?: ReactNode
 }
 
 export const SnapshotWrapper = ({ children }: SnapshotWrapperProps): JSX.Element => {
-  return <Stack alignItems="start">{children}</Stack>
+  return (
+    <>
+      <EinrideProvider theme={einrideTheme} colorMode="light">
+        <Wrapper>
+          <Stack alignItems="start">{children}</Stack>
+        </Wrapper>
+      </EinrideProvider>
+      <EinrideProvider theme={einrideTheme} colorMode="dark">
+        <Wrapper>
+          <Stack alignItems="start">{children}</Stack>
+        </Wrapper>
+      </EinrideProvider>
+    </>
+  )
 }
+
+const Wrapper = styled.div`
+  padding: ${({ theme }) => 3 * theme.spacingBase}rem;
+  // set body styles
+  background: ${({ theme }) => theme.colors.background.primary};
+  color: ${({ theme }) => theme.colors.content.primary};
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+`
