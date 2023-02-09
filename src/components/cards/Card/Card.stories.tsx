6a6381fa846eb2358d57f1743192c63e7aa9e984
Filip Tammergård <filip@tammergard.se>
2023-02-09 15:51:07 +0100

    chore: convert card stories to CSF3

--- src/components/cards/Card/Card.stories.tsx
+++ src/components/cards/Card/Card.stories.tsx
@@ -1,4 +1,5 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react"
+import { SnapshotWrapper } from "../../../lib/storybook/SnapshotWrapper"
 import { StepGauge } from "../../charts/StepGauge/StepGauge"
 import { StepProgress } from "../../charts/StepProgress/StepProgress"
 import { Icon } from "../../content/Icon/Icon"
@@ -11,90 +12,107 @@ import { Card } from "./Card"
 export default {
   title: "Cards/Card",
   component: Card,
-} as ComponentMeta<typeof Card>
+} satisfies ComponentMeta<typeof Card>
 
-const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />
+type Story = ComponentStoryObj<typeof Card>
 
-export const Gauge = Template.bind({})
-Gauge.args = {
-  background: "secondary",
-  children: (
-    <Group justifyContent="space-between">
-      <Stack gap="none">
-        <Text color="secondary">Electrification potential</Text>
-        <Text>Good</Text>
+export const Gauge = {
+  args: {
+    background: "secondary",
+    children: (
+      <Group justifyContent="space-between">
+        <Stack gap="none">
+          <Text color="secondary">Electrification potential</Text>
+          <Text>Good</Text>
+        </Stack>
+        <StepGauge aria-label="Electrification potential" steps={3} completedSteps={2} />
+      </Group>
+    ),
+  },
+} satisfies Story
+
+export const Progress = {
+  args: {
+    background: "secondary",
+    paddingBlockEnd: "sm",
+    children: (
+      <Stack gap="sm">
+        <Group justifyContent="space-between">
+          <Text>
+            Sweden <Icon name="arrowRight" /> Germany
+          </Text>
+          <Text color="positive">In transit</Text>
+        </Group>
+        <StepProgress
+          aria-label="Progress on transit between Sweden and Germany"
+          completedSteps={2}
+        />
       </Stack>
-      <StepGauge aria-label="Electrification potential" steps={3} completedSteps={2} />
-    </Group>
-  ),
-}
+    ),
+  },
+} satisfies Story
 
-export const Progress = Template.bind({})
-Progress.args = {
-  background: "secondary",
-  paddingBlockEnd: "sm",
-  children: (
-    <Stack gap="sm">
-      <Group justifyContent="space-between">
-        <Text>
-          Sweden <Icon name="arrowRight" /> Germany
+export const Stat = {
+  args: {
+    background: "secondary",
+    children: (
+      <Stack height={22} justifyContent="space-between">
+        <Group justifyContent="space-between">
+          <Text color="secondary">Emissions</Text>
+          <Text color="secondary">
+            <Text as="span" color="positive">
+              <Icon name="arrowDown" />
+              3%
+            </Text>{" "}
+            this year
+          </Text>
+        </Group>
+        <Text as="p" variant="titleLg">
+          230 tn
         </Text>
-        <Text color="positive">In transit</Text>
-      </Group>
-      <StepProgress
-        aria-label="Progress on transit between Sweden and Germany"
-        completedSteps={2}
-      />
-    </Stack>
-  ),
-}
+      </Stack>
+    ),
+  },
+} satisfies Story
 
-export const Stat = Template.bind({})
-Stat.args = {
-  background: "secondary",
-  children: (
-    <Stack height={22} justifyContent="space-between">
-      <Group justifyContent="space-between">
-        <Text color="secondary">Emissions</Text>
-        <Text color="secondary">
-          <Text as="span" color="positive">
-            <Icon name="arrowDown" />
-            3%
-          </Text>{" "}
-          this year
-        </Text>
-      </Group>
-      <Text as="p" variant="titleLg">
-        230 tn
-      </Text>
-    </Stack>
-  ),
-}
+export const InsideCard = {
+  args: {
+    background: "secondary",
+    children: (
+      <Stack>
+        <Group justifyContent="space-between">
+          <Text>Slot</Text>
+          <Text>Slot</Text>
+        </Group>
+        <Card background="secondary" borderRadius="sm">
+          <Text color="secondary">Electrification potential</Text>
+          <Text>Good</Text>
+        </Card>
+      </Stack>
+    ),
+  },
+} satisfies Story
 
-export const InsideCard = Template.bind({})
-InsideCard.args = {
-  background: "secondary",
-  children: (
-    <Stack>
-      <Group justifyContent="space-between">
+export const WithInput = {
+  args: {
+    background: "secondary",
+    children: (
+      <Stack>
         <Text>Slot</Text>
-        <Text>Slot</Text>
-      </Group>
-      <Card background="secondary" borderRadius="sm">
-        <Text color="secondary">Electrification potential</Text>
-        <Text>Good</Text>
-      </Card>
-    </Stack>
-  ),
-}
+        <TextInput label="Label" />
+      </Stack>
+    ),
+  },
+} satisfies Story
 
-export const WithInput = Template.bind({})
-WithInput.args = {
-  background: "secondary",
-  children: (
-    <Stack>
-      <Text>Slot</Text>
-      <TextInput label="Label" />
-    </Stack>
+export const Snapshot = {
+  render: () => (
+    <SnapshotWrapper alignItems="stretch">
+      {[Gauge, Progress, Stat, InsideCard, WithInput].map((Story, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Card key={index} {...Story.args} />
+      ))}
+    </SnapshotWrapper>
   ),
-}
+  parameters: { chromatic: { disableSnapshot: false } },
+} satisfies Story
--- src/lib/storybook/SnapshotWrapper.tsx
+++ src/lib/storybook/SnapshotWrapper.tsx
@@ -1,24 +1,29 @@
 import styled from "@emotion/styled"
 import { ReactNode } from "react"
+import { BoxProps } from "../../components/layout/Box/Box"
 import { Stack } from "../../components/layout/Stack/Stack"
 import { EinrideProvider } from "../../contexts/EinrideProvider"
 import { einrideTheme } from "../theme/einride"
 
-interface SnapshotWrapperProps {
+interface SnapshotWrapperProps extends BoxProps {
   children?: ReactNode
 }
 
-export const SnapshotWrapper = ({ children }: SnapshotWrapperProps): JSX.Element => {
+export const SnapshotWrapper = ({ children, ...props }: SnapshotWrapperProps): JSX.Element => {
   return (
     <>
       <EinrideProvider theme={einrideTheme} colorMode="light">
         <Wrapper>
-          <Stack alignItems="start">{children}</Stack>
+          <Stack alignItems="start" {...props}>
+            {children}
+          </Stack>
         </Wrapper>
       </EinrideProvider>
       <EinrideProvider theme={einrideTheme} colorMode="dark">
         <Wrapper>
-          <Stack alignItems="start">{children}</Stack>
+          <Stack alignItems="start" {...props}>
+            {children}
+          </Stack>
         </Wrapper>
       </EinrideProvider>
     </>
