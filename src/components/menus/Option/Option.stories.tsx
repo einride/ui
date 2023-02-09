0b8655ad857d84302f1e5428368323d28a93c2bd
Filip Tammergård <filip@tammergard.se>
2023-02-09 15:10:51 +0100

    chore: convert menu stories to CSF3

--- src/components/menus/Option/Option.stories.tsx
+++ src/components/menus/Option/Option.stories.tsx
@@ -1,40 +1,45 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react"
 import { Icon } from "../../content/Icon/Icon"
 import { Option } from "./Option"
 
 export default {
   title: "Menus/Option",
   component: Option,
-} as ComponentMeta<typeof Option>
+} satisfies ComponentMeta<typeof Option>
 
-const Template: ComponentStory<typeof Option> = (args) => <Option {...args} />
+type Story = ComponentStoryObj<typeof Option>
 
-export const Basic = Template.bind({})
-Basic.args = {
-  children: "Option",
-  icon: <Icon name="arrowRight" />,
-}
+export const Basic = {
+  args: {
+    children: "Option",
+    icon: <Icon name="arrowRight" />,
+  },
+} satisfies Story
 
-export const Selected = Template.bind({})
-Selected.args = {
-  ...Basic.args,
-  selected: true,
-}
+export const Selected = {
+  args: {
+    ...Basic.args,
+    selected: true,
+  },
+} satisfies Story
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-  ...Basic.args,
-  variant: "secondary",
-}
+export const Secondary = {
+  args: {
+    ...Basic.args,
+    variant: "secondary",
+  },
+} satisfies Story
 
-export const SecondarySelected = Template.bind({})
-SecondarySelected.args = {
-  ...Secondary.args,
-  selected: true,
-}
+export const SecondarySelected = {
+  args: {
+    ...Secondary.args,
+    selected: true,
+  },
+} satisfies Story
 
-export const AsButton = Template.bind({})
-AsButton.args = {
-  ...Basic.args,
-  as: "button",
-}
+export const AsButton = {
+  args: {
+    ...Basic.args,
+    as: "button",
+  },
+} satisfies Story
