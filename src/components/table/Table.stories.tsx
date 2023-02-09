c904bf5a68caac11efa0708e77359c66a96338b0
Filip Tammergård <filip@tammergard.se>
2023-02-09 14:56:06 +0100

    chore: convert table story to CSF3

--- src/components/table/Table.stories.tsx
+++ src/components/table/Table.stories.tsx
@@ -1,5 +1,5 @@
 import { expect } from "@storybook/jest"
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react"
 import { within } from "@storybook/testing-library"
 import { Table } from "./Table/Table"
 import { Tbody } from "./Tbody/Tbody"
@@ -11,55 +11,62 @@ import { Tr } from "./Tr/Tr"
 export default {
   title: "Table/Table",
   component: Table,
-} as ComponentMeta<typeof Table>
+} satisfies ComponentMeta<typeof Table>
 
-export const Basic: ComponentStory<typeof Table> = (args) => (
-  <Table {...args}>
-    <Thead>
-      <Tr color="secondary">
-        <Th scope="col">Country</Th>
-        <Th scope="col">Capital</Th>
-        <Th scope="col">Date format</Th>
-        <Th scope="col">Internet TLD</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Th scope="row">Sweden</Th>
-        <Td>Stockholm</Td>
-        <Td>YYYY-MM-DD</Td>
-        <Td>.se</Td>
-      </Tr>
-      <Tr>
-        <Th scope="row">Germany</Th>
-        <Td>Berlin</Td>
-        <Td>DD.MM.YYYY</Td>
-        <Td>.de</Td>
-      </Tr>
-      <Tr>
-        <Th scope="row">Brazil</Th>
-        <Td>Brasília</Td>
-        <Td>DD/MM/YYYY</Td>
-        <Td>.br</Td>
-      </Tr>
-    </Tbody>
-  </Table>
-)
+type Story = ComponentStoryObj<typeof Table>
 
-Basic.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const table = canvas.getByRole("table")
-  await expect(table).toBeInTheDocument()
-  const columnHeaders = canvas.getAllByRole("columnheader")
-  await expect(columnHeaders).toHaveLength(4)
-  const firstColumnHeader = columnHeaders[0]
-  await expect(firstColumnHeader).toHaveAccessibleName(/country/i)
-  const rowHeaders = canvas.getAllByRole("rowheader")
-  await expect(rowHeaders).toHaveLength(3)
-  const firstRowHeader = rowHeaders[0]
-  await expect(firstRowHeader).toHaveAccessibleName(/sweden/i)
-  const cells = canvas.getAllByRole("cell")
-  await expect(cells).toHaveLength(9)
-  const firstCell = cells[0]
-  await expect(firstCell).toHaveAccessibleName(/stockholm/i)
+const Template = (): JSX.Element => {
+  return (
+    <Table>
+      <Thead>
+        <Tr color="secondary">
+          <Th scope="col">Country</Th>
+          <Th scope="col">Capital</Th>
+          <Th scope="col">Date format</Th>
+          <Th scope="col">Internet TLD</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Th scope="row">Sweden</Th>
+          <Td>Stockholm</Td>
+          <Td>YYYY-MM-DD</Td>
+          <Td>.se</Td>
+        </Tr>
+        <Tr>
+          <Th scope="row">Germany</Th>
+          <Td>Berlin</Td>
+          <Td>DD.MM.YYYY</Td>
+          <Td>.de</Td>
+        </Tr>
+        <Tr>
+          <Th scope="row">Brazil</Th>
+          <Td>Brasília</Td>
+          <Td>DD/MM/YYYY</Td>
+          <Td>.br</Td>
+        </Tr>
+      </Tbody>
+    </Table>
+  )
 }
+
+export const Basic = {
+  render: () => <Template />,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const table = canvas.getByRole("table")
+    await expect(table).toBeInTheDocument()
+    const columnHeaders = canvas.getAllByRole("columnheader")
+    await expect(columnHeaders).toHaveLength(4)
+    const firstColumnHeader = columnHeaders[0]
+    await expect(firstColumnHeader).toHaveAccessibleName(/country/i)
+    const rowHeaders = canvas.getAllByRole("rowheader")
+    await expect(rowHeaders).toHaveLength(3)
+    const firstRowHeader = rowHeaders[0]
+    await expect(firstRowHeader).toHaveAccessibleName(/sweden/i)
+    const cells = canvas.getAllByRole("cell")
+    await expect(cells).toHaveLength(9)
+    const firstCell = cells[0]
+    await expect(firstCell).toHaveAccessibleName(/stockholm/i)
+  },
+} satisfies Story
