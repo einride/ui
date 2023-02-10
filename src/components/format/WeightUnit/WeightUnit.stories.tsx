2e149874d30f68afc88858dda496f9fbefe4a60e
Filip Tammergård <filip@tammergard.se>
2023-02-10 11:56:02 +0100

    chore: convert format stories to CSF3

--- src/components/format/WeightUnit/WeightUnit.stories.tsx
+++ src/components/format/WeightUnit/WeightUnit.stories.tsx
@@ -1,44 +1,47 @@
 import { expect } from "@storybook/jest"
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react"
 import { within } from "@storybook/testing-library"
 import { WeightUnit } from "./WeightUnit"
 
 export default {
   title: "Format/WeightUnit",
   component: WeightUnit,
-} as ComponentMeta<typeof WeightUnit>
+} satisfies ComponentMeta<typeof WeightUnit>
 
-const Template: ComponentStory<typeof WeightUnit> = (args) => <WeightUnit {...args} />
+type Story = ComponentStoryObj<typeof WeightUnit>
 
-export const Metric = Template.bind({})
-Metric.args = {
-  locales: "en-US",
-  unitSystem: "metric",
-}
-Metric.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const unit = canvas.getByText("kg")
-  expect(unit).toBeInTheDocument()
-}
+export const Metric = {
+  args: {
+    locales: "en-US",
+    unitSystem: "metric",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const unit = canvas.getByText("kg")
+    expect(unit).toBeInTheDocument()
+  },
+} satisfies Story
 
-export const Imperial = Template.bind({})
-Imperial.args = {
-  locales: "en-US",
-  unitSystem: "imperial",
-}
-Imperial.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const unit = canvas.getByText("lb")
-  expect(unit).toBeInTheDocument()
-}
+export const Imperial = {
+  args: {
+    locales: "en-US",
+    unitSystem: "imperial",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const unit = canvas.getByText("lb")
+    expect(unit).toBeInTheDocument()
+  },
+} satisfies Story
 
-export const ImperialInSwedish = Template.bind({})
-ImperialInSwedish.args = {
-  locales: "sv-SE",
-  unitSystem: "imperial",
-}
-ImperialInSwedish.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const unit = canvas.getByText("pund")
-  expect(unit).toBeInTheDocument()
-}
+export const ImperialInSwedish = {
+  args: {
+    locales: "sv-SE",
+    unitSystem: "imperial",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const unit = canvas.getByText("pund")
+    expect(unit).toBeInTheDocument()
+  },
+} satisfies Story
