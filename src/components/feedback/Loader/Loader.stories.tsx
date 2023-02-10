f80224096a7f5b14cbbcb19f7f98b7d8ecec704a
Filip Tammergård <filip@tammergard.se>
2023-02-10 12:00:17 +0100

    chore: convert feedback stories to CSF3

--- src/components/feedback/Loader/Loader.stories.tsx
+++ src/components/feedback/Loader/Loader.stories.tsx
@@ -1,12 +1,11 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react"
 import { Loader } from "./Loader"
 
 export default {
   title: "Feedback/Loader",
   component: Loader,
-} as ComponentMeta<typeof Loader>
+} satisfies ComponentMeta<typeof Loader>
 
-const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />
+type Story = ComponentStoryObj<typeof Loader>
 
-export const Default = Template.bind({})
-Default.args = {}
+export const Default = {} satisfies Story
