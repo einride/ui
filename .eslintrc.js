3b5425bf92496889af5f33bbafea3e93cc592da7
Filip Tammergård <filip@tammergard.se>
2023-03-27 13:02:03 +0200

    chore: review packages separately

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,15 @@ module.exports = {
   rules: {
     "react/jsx-props-no-spreading": "off", // props spreading is good practice in a component library
   },
+  overrides: [
+    // Allow extraneous dependencies in test files
+    {
+      files: [
+        "**/*.test.{ts,tsx}", // test file
+      ],
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+      },
+    },
+  ],
 }
--- packages/einride-ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import { defineConfig } from "vite"
-
-export default defineConfig({
-  test: {
-    environment: "happy-dom",
-  },
-})
