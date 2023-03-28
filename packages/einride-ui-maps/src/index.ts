a2304c1d39ff0c90235d7b2e752c24fbb709274f
Filip Tammergård <filip@tammergard.se>
2023-03-28 16:23:46 +0200

    fix: add dark charger icon

--- packages/einride-ui-maps/src/index.ts
+++ packages/einride-ui-maps/src/index.ts
@@ -1,5 +1,6 @@
 import ChargerIcon from "./assets/charger.svg"
+import ChargerIconDark from "./assets/chargerDark.svg"
 
 export { MapTooltip } from "./components/MapTooltip/MapTooltip"
 export * from "./styles"
-export { ChargerIcon }
+export { ChargerIcon, ChargerIconDark }
