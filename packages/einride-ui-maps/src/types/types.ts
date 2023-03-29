142676ff7508e2bff915a340eecfc4edbcafe97b
Filip Tammergård <filip@tammergard.se>
2023-03-29 14:45:31 +0200

    fix(maps): add marker hooks

--- packages/einride-ui-maps/src/types/types.ts
+++ packages/einride-ui-maps/src/types/types.ts
@@ -9,3 +9,25 @@ export interface IconMask {
   width: number
   mask: boolean
 }
+
+export interface MarkerMask {
+  url: string
+  height: number
+  width: number
+  mask?: boolean
+  anchorY?: number
+  anchorX?: number
+}
+
+export type MarkerName =
+  | "siteWithCharger"
+  | "site"
+  | "siteElectric"
+  | "hub"
+  | "hubWithCharger"
+  | "hubElectric"
+  | "charger"
+export type MarkerState = "hover" | "selected" | "default"
+
+export type VehicleMarkerName = "aet" | "cet"
+export type VehicleMarkerState = "warning" | "default"
