969e09490e2abbc726ca737bbf17b552f1cf7b3c
Filip Tammergård <filip@tammergard.se>
2023-03-30 13:57:29 +0200

    fix: remove unsued exports

--- packages/einride-ui/src/components/mapping/index.ts
+++ packages/einride-ui/src/components/mapping/index.ts
@@ -5,14 +5,11 @@ import ChargerSmallIconDark from "./chargerSmallDark.svg"
 import ConventionalTruckIcon from "./conventionalTruck.svg"
 import PodIcon from "./pod.svg"
 import SiteIcon from "./site.svg"
-import SiteIconDark from "./siteDark.svg"
 import SiteHoveredIcon from "./siteHovered.svg"
 import SiteSelectedIcon from "./siteSelected.svg"
-import SiteSelectedIconDark from "./siteSelectedDark.svg"
 import SiteSmallIcon from "./siteSmall.svg"
 import SiteSmallIconDark from "./siteSmallDark.svg"
 import SiteWithChargerIcon from "./siteWithCharger.svg"
-import SiteWithChargerIconDark from "./siteWithChargerDark.svg"
 import SiteWithChargerSmall from "./siteWithChargerSmall.svg"
 import SiteWithChargerSmallDark from "./siteWithChargerSmallDark.svg"
 
@@ -46,40 +43,5 @@ export const iconMask: IconMask = {
   url: SiteIcon,
   height: 96,
   width: 96,
-  mask: false,
-}
-
-export const hoverIconMask: IconMask = {
-  url: SiteHoveredIcon,
-  height: 96,
-  width: 96,
-  mask: false,
-}
-
-export const selectIconMask: IconMask = {
-  url: SiteSelectedIcon,
-  height: 96,
-  width: 96,
-  mask: false,
-}
-
-export const iconMaskDark: IconMask = {
-  url: SiteIconDark,
-  height: 96,
-  width: 96,
-  mask: false,
-}
-
-export const selectIconMaskDark: IconMask = {
-  url: SiteSelectedIconDark,
-  height: 96,
-  width: 96,
-  mask: false,
-}
-
-export const siteWithChargerIconMaskDark: IconMask = {
-  url: SiteWithChargerIconDark,
-  height: 80,
-  width: 128,
   mask: false,
 }
