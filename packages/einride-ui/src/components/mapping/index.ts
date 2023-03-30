c275501aa0575772b720b376de5112928e15f472
Filip Tammergård <filip@tammergard.se>
2023-03-30 14:11:11 +0200

    fix: remove unsued exports

--- packages/einride-ui/src/components/mapping/index.ts
+++ packages/einride-ui/src/components/mapping/index.ts
@@ -1,47 +1,13 @@
-import ChargerIcon from "./charger.svg"
-import ChargerIconDark from "./chargerDark.svg"
-import ChargerSmallIcon from "./chargerSmall.svg"
-import ChargerSmallIconDark from "./chargerSmallDark.svg"
-import ConventionalTruckIcon from "./conventionalTruck.svg"
 import PodIcon from "./pod.svg"
 import SiteIcon from "./site.svg"
-import SiteHoveredIcon from "./siteHovered.svg"
-import SiteSelectedIcon from "./siteSelected.svg"
-import SiteSmallIcon from "./siteSmall.svg"
-import SiteSmallIconDark from "./siteSmallDark.svg"
-import SiteWithChargerIcon from "./siteWithCharger.svg"
-import SiteWithChargerSmall from "./siteWithChargerSmall.svg"
-import SiteWithChargerSmallDark from "./siteWithChargerSmallDark.svg"
 
 import { IconMask } from "./types/types"
 
-export {
-  ChargerIcon,
-  ChargerIconDark,
-  ChargerSmallIcon,
-  ChargerSmallIconDark,
-  ConventionalTruckIcon,
-  PodIcon,
-  SiteHoveredIcon,
-  SiteIcon,
-  SiteSelectedIcon,
-  SiteSmallIcon,
-  SiteSmallIconDark,
-  SiteWithChargerIcon,
-  SiteWithChargerSmall,
-  SiteWithChargerSmallDark,
-}
+export { PodIcon }
 
 export const coloredIconMask: IconMask = {
   url: SiteIcon,
   height: 96,
   width: 96,
   mask: true,
-}
-
-export const iconMask: IconMask = {
-  url: SiteIcon,
-  height: 96,
-  width: 96,
-  mask: false,
 }
