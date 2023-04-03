32350b1f0420d21f669ddbafa2008179be5d66f0
Filip Tammergård <filip@tammergard.se>
2023-04-03 09:18:12 +0200

    chore(Logo): add docs

--- packages/einride-ui/src/components/content/Logo/Logo.stories.tsx
+++ packages/einride-ui/src/components/content/Logo/Logo.stories.tsx
@@ -1,7 +1,10 @@
+import { expect } from "@storybook/jest"
 import { Meta, StoryObj } from "@storybook/react"
+import { within } from "@storybook/testing-library"
 import { SnapshotWrapper } from "../../../lib/storybook/SnapshotWrapper"
 import { Logo } from "./Logo"
 
+/** Einride's official logo. */
 const meta = {
   component: Logo,
 } satisfies Meta<typeof Logo>
@@ -9,26 +12,42 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default = {
+export const Basic = {
   args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const logo = canvas.getByRole("img")
+    await expect(logo).toHaveAccessibleName(/einride/i)
+  },
 } satisfies Story
 
 export const Small = {
   args: {
     size: "sm",
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const logo = canvas.getByRole("img")
+    await expect(logo).toHaveAccessibleName(/einride/i)
+  },
 } satisfies Story
 
+/** It's possible to make the logo larger. */
 export const Large = {
   args: {
     size: "lg",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const logo = canvas.getByRole("img")
+    await expect(logo).toHaveAccessibleName(/einride/i)
   },
 } satisfies Story
 
 export const Snapshot = {
   render: () => (
     <SnapshotWrapper>
-      {[Default, Small, Large].map((Story, index) => (
+      {[Basic, Small, Large].map((Story, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <Logo key={index} {...Story.args} />
       ))}
