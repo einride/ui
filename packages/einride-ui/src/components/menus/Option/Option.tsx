dba167f9ff633d734db0068e411e35958a62e216
Filip Tammergård <filip@tammergard.se>
2023-04-27 12:56:16 +0200

    feat(Option): enable passing children flexibly without disturbance from icon prop

--- packages/einride-ui/src/components/menus/Option/Option.tsx
+++ packages/einride-ui/src/components/menus/Option/Option.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled"
 import { ComponentPropsWithoutRef, ElementType, forwardRef, ReactNode } from "react"
-import { Icon } from "../../content/Icon/Icon"
 
 export interface OptionProps extends ComponentPropsWithoutRef<"div"> {
   /** Rendered element. */
@@ -9,32 +8,15 @@ export interface OptionProps extends ComponentPropsWithoutRef<"div"> {
   /** Option content. */
   children: ReactNode
 
-  /** Icon shown at the end of the option row. */
-  icon?: ReactNode
-
-  /** Whether the option is selected or not. */
-  selected?: boolean
-
-  /** Whether the option is focused (e.g. via arrow keys) or not. */
-  focused?: boolean
-
   /** Variant of the option. Default is `primary`. */
   variant?: Variant
 }
 
 export const Option = forwardRef<HTMLDivElement, OptionProps>(
-  ({ children, icon, selected, focused, variant = "primary", ...props }, forwardedRef) => {
+  ({ children, variant = "primary", ...props }, forwardedRef) => {
     return (
-      <Wrapper
-        aria-selected={selected}
-        data-focused={focused}
-        variant={variant}
-        tabIndex={0}
-        {...props}
-        ref={forwardedRef}
-      >
+      <Wrapper variant={variant} tabIndex={0} {...props} ref={forwardedRef}>
         {children}
-        {selected ? <Icon name="checkmark" /> : icon}
       </Wrapper>
     )
   },
@@ -52,7 +34,7 @@ const Wrapper = styled.div<WrapperProps>`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding-block: ${({ theme }) => 1.25 * theme.spacer}px;
+  block-size: ${({ theme }) => 6 * theme.spacingBase}rem;
   padding-inline: ${({ theme }) => 1 * theme.spacer}px;
   color: ${({ theme }) => theme.colors.content.primary};
   border-radius: ${({ theme }) => theme.borderRadii.sm};
@@ -61,24 +43,22 @@ const Wrapper = styled.div<WrapperProps>`
   min-block-size: ${({ theme }) => 6 * theme.spacingBase}rem;
   column-gap: ${({ theme }) => 2 * theme.spacingBase}rem;
   inline-size: 100%;
-
   background: ${({ variant, theme }) =>
     variant === "secondary"
       ? theme.colors.background.secondaryElevated
       : theme.colors.background.primary};
 
-  &:focus-visible {
-    outline: none;
-    background: ${({ theme }) => theme.colors.background.tertiary};
-    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.border.selected};
-  }
-
   &:hover,
+  &:focus-visible,
   &[data-focused="true"] {
     background: ${({ variant, theme }) =>
       variant === "secondary"
         ? theme.colors.background.tertiary
         : theme.colors.background.secondaryElevated};
-    box-shadow: none;
+  }
+
+  &:focus-visible {
+    outline: none;
+    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.border.selected};
   }
 `
