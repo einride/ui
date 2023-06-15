0dba7c80e8d7533f5b0a223757ea3e237bec2d27
Filip Tammergård <filip@tammergard.se>
2023-06-15 14:26:08 +0200

    chore: improve menu docs

--- packages/einride-ui/src/components/menus/Menu/Menu.stories.tsx
+++ packages/einride-ui/src/components/menus/Menu/Menu.stories.tsx
@@ -1,9 +1,10 @@
 import { expect } from "@storybook/jest"
 import { Meta, StoryObj } from "@storybook/react"
 import { userEvent, within } from "@storybook/testing-library"
-import { useState } from "react"
+import { ComponentProps, useState } from "react"
 import { Icon } from "../../content/Icon/Icon"
 import { IconButton } from "../../controls/buttons/IconButton/IconButton"
+import { Box } from "../../layout/Box/Box"
 import { Table } from "../../table/Table/Table"
 import { Tbody } from "../../table/Tbody/Tbody"
 import { Td } from "../../table/Td/Td"
@@ -22,12 +23,14 @@ const meta = {
 export default meta
 
 interface TemplateProps {
+  align?: ComponentProps<typeof MenuContent>["align"]
   defaultOpen?: boolean
   withOverlay?: boolean
   inPortal?: boolean
 }
 
 const Template = ({
+  align = "center",
   defaultOpen = false,
   withOverlay = false,
   inPortal = true,
@@ -36,7 +39,7 @@ const Template = ({
     <MenuTrigger>
       <IconButton aria-label="See options" icon="ellipsis" />
     </MenuTrigger>
-    <MenuContent withOverlay={withOverlay} inPortal={inPortal}>
+    <MenuContent align={align} withOverlay={withOverlay} inPortal={inPortal}>
       <MenuItem icon={<Icon name="arrowDownCircle" />}>Option 1</MenuItem>
       <MenuItem icon={<Icon name="arrowDownCircle" />}>Option 2</MenuItem>
       <MenuItem>Option 3</MenuItem>
@@ -78,6 +81,20 @@ export const WithoutPortal = {
     const canvas = within(canvasElement.parentElement ?? canvasElement)
     const menu = canvas.getByRole("menu", { name: "See options" })
     await expect(menu).toBeInTheDocument()
+  },
+} satisfies StoryObj
+
+/** Menu content can be aligned against the menu trigger with `align` prop. */
+export const AlignContentEnd = {
+  render: () => (
+    <Box display="flex" justifyContent="center">
+      <Template align="end" />
+    </Box>
+  ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement.parentElement ?? canvasElement)
+    const button = canvas.getByRole("button", { name: "See options" })
+    await expect(button).toBeInTheDocument()
   },
 } satisfies StoryObj
 
