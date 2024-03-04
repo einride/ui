e2ded3ee61360ea6ac4bf88e6fe826ebf59ea752
Filip Tammergård <filip@tammergard.se>
2024-03-04 12:15:36 +0100

    chore: remove deprecated packages

--- packages/einride-ui-dates/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { DatePicker } from "./components/DatePicker/DatePicker"
-export { DatePickerInput } from "./components/DatePickerInput/DatePickerInput"
-export {
-  DateRangePicker,
-  type DateRangePickerValue,
-} from "./components/DateRangePicker/DateRangePicker"
-export { DateRangePickerInput } from "./components/DateRangePickerInput/DateRangePickerInput"
